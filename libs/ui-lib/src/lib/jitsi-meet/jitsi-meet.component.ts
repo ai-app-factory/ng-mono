9f30219184206558771236d358ed06c19f8484db
sukrut29 <sukrutssathaye@gmail.com>
2024-09-30 14:56:50 +0200

    generate jitsi-meet component and add the html template required to launch a meeting

---
+++ libs/ui-lib/src/lib/jitsi-meet/jitsi-meet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-jitsi-meet',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './jitsi-meet.component.html',
+  styleUrl: './jitsi-meet.component.css',
+})
+export class JitsiMeetComponent {}
