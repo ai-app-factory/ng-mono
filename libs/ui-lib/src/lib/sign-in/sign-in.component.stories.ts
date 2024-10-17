65d8b805b6a1479e92b3e4ad21252b11da69e45f
sukrut29 <sukrutssathaye@gmail.com>
2024-10-17 10:45:26 +0200

    create a mockAuth to mock the functions of @angular/fire. Error prone jest not found

--- libs/ui-lib/src/lib/sign-in/sign-in.component.stories.ts
+++ libs/ui-lib/src/lib/sign-in/sign-in.component.stories.ts
@@ -4,6 +4,11 @@ import { AUTH } from './auth.token';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
+const mockAuth = {
+  signInWithEmailAndPassword: jest.fn().mockResolvedValue({ user: { uid: '12345' } }),
+  createUserWithEmailAndPassword: jest.fn().mockResolvedValue({ user: { uid: '12345' } }),
+  signInWithPopup: jest.fn().mockResolvedValue({ user: { uid: '12345' } }),
+};
 const meta: Meta<SignInComponent> = {
   component: SignInComponent,
   title: 'SignInComponent',
