3ab0599802b919f681e9da264a384cdb9c22c54a
sukrut29 <sukrutssathaye@gmail.com>
2024-10-17 11:57:14 +0200

    add exports for components added

--- libs/ui-lib/src/index.ts
+++ libs/ui-lib/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/ui-lib/ui-lib.component';
+export * from './lib/welcome-page/welcome-page.component';
+export * from './lib/sign-in/sign-in.component';
+export * from './lib/jitsi-meet/jitsi-meet.component';
+
