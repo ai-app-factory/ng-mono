f148fd1f16626191d5c01aa75de2ce9a917a7607
sukrut29 <sukrutssathaye@gmail.com>
2024-10-17 10:46:12 +0200

    change the injection method

--- libs/ui-lib/src/lib/sign-in/sign-in.component.ts
+++ libs/ui-lib/src/lib/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
