45f06957428be9ee0da5b13e574e70d0dc663cc2
sukrut29 <sukrutssathaye@gmail.com>
2024-10-19 18:50:29 +0200

    add check-box component with subtasks as well with basic update feature

--- libs/mui-lib/src/lib/check-box/check-box.component.stories.ts
+++ libs/mui-lib/src/lib/check-box/check-box.component.stories.ts
@@ -13,12 +13,62 @@ type Story = StoryObj<CheckBoxComponent>;
 
 export const Primary: Story = {
   args: {},
-};
-
-export const Heading: Story = {
-  args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/check-box works!/gi)).toBeTruthy();
+    expect(canvas.getByText("Task 1")).toBeTruthy();
+  },
+};
+
+export const DisabledCheckbox: Story = {
+  args: {
+    disabled: true,
+    task: { name: 'Task 1', completed: false },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Task 1")).toBeTruthy();
+  },
+
+};
+
+export const CheckedCheckbox: Story = {
+  args: {
+    checked: true,
+    task: { name: 'Task 1', completed: true },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Task 1")).toBeTruthy();
+  },
+};
+
+export const DisabledAndCheckedCheckbox: Story = {
+  args: {
+    checked: true,
+    disabled: true,
+    task: { name: 'Task 1', completed: true },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Task 1")).toBeTruthy();
+  },
+};
+
+export const CheckboxWithSubtasks: Story = {
+  args: {
+    task: {
+      name: 'Task 1',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: true },
+      ],
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Task 1")).toBeTruthy();
+    expect(canvas.getByText("Subtask 1")).toBeTruthy();
+    expect(canvas.getByText("Subtask 2")).toBeTruthy();
   },
 };
--- libs/mui-lib/src/lib/check-box/check-box.component.ts
+++ libs/mui-lib/src/lib/check-box/check-box.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  computed,
+  signal,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'lib-check-box',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    FormsModule
+  ],
   templateUrl: './check-box.component.html',
-  styleUrl: './check-box.component.scss',
+  styleUrls: ['./check-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CheckBoxComponent {}
+export class CheckBoxComponent {
+  @Input() checked: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() task: Task = {
+    name: 'Task 1', completed: false,
+    subtasks: [
+      { name: 'Subtask 1', completed: false },
+      { name: 'Subtask 2', completed: false },
+      { name: 'Subtask 3', completed: true }
+    ]
+  };
+
+  readonly partiallyComplete = computed(() => {
+    if (!this.task.subtasks) {
+      return false;
+    }
+    return this.task.subtasks.some(t => t.completed) && !this.task.subtasks.every(t => t.completed);
+  });
+
+  update(completed: boolean, index?: number) {
+    if (index === undefined) {
+      this.task.completed = completed;
+      this.task.subtasks?.forEach(subtask => subtask.completed = completed);
+    } else {
+      this.task.subtasks![index].completed = completed;
+      this.task.completed = this.task.subtasks?.every(subtask => subtask.completed) ?? false;
+    }
+  }
+}
