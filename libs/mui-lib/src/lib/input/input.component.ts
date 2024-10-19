ecfd61774e2e115710e85e5bc7570200dc746f68
sukrut29 <sukrutssathaye@gmail.com>
2024-10-19 19:59:10 +0200

    add output from input component

--- libs/mui-lib/src/lib/input/input.component.ts
+++ libs/mui-lib/src/lib/input/input.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'lib-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
-export class InputComponent {}
+export class InputComponent {
+  @Output() value: string = '';
+  @Output() placeholder: string = 'Enter text';
+  
+}
