f3ed92c220ddc215893c73cb3468b5547c72ee84
sukrut29 <sukrutssathaye@gmail.com>
2024-10-19 15:12:59 +0200

    auto generate stater code and files for flat-button component using @nx/angular:component generator

---
+++ libs/mui-lib/src/lib/flat-button/flat-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-flat-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flat-button.component.html',
+  styleUrl: './flat-button.component.scss',
+})
+export class FlatButtonComponent {}
