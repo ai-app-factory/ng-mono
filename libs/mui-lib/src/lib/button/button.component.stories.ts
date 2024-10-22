ea78a85bfc8c03222fc0956c708482c3275c2e87
sukrut29 <sukrutssathaye@gmail.com>
2024-10-22 17:40:47 +0200

    add button component with buttonType for selecting different types of buttons.

---
+++ libs/mui-lib/src/lib/button/button.component.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ButtonComponent, ButtonType } from './button.component';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+const meta: Meta<ButtonComponent> = {
+  component: ButtonComponent,
+  title: 'ButtonComponent',
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    buttonType: {
+      options: [
+        ButtonType.RAISED,
+        ButtonType.FAB,
+        ButtonType.BUTTON,
+        ButtonType.FLAT,
+        ButtonType.STROKED
+      ],
+      control: {
+        type: 'radio',
+      },
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+};
+
+export const CustomLabel: Story = {
+  args: {
+    disabled: false,
+    label: "this is the label text"
+  }
+};
+
+export const FabButton: Story = {
+  args: {
+    disabled: false,
+    buttonType: ButtonType.FAB
+  }
+};
+
+export const FlatButton: Story = {
+  args: {
+    disabled: false,
+    buttonType: ButtonType.FLAT
+  }
+};
+
+export const StrokedButton: Story = {
+  args: {
+    disabled: false,
+    buttonType: ButtonType.STROKED
+  }
+};
+
+export const NormalButton: Story = {
+  args: {
+    disabled: false,
+    buttonType: ButtonType.BUTTON
+  }
+};
+
+export const RaisedButton: Story = {
+  args: {
+    disabled: false,
+    buttonType: ButtonType.RAISED
+  }
+};
+
+export const RaisedDisabled: Story = {
+  args: {
+    disabled: true,
+    buttonType: ButtonType.RAISED
+  }
+};
+
