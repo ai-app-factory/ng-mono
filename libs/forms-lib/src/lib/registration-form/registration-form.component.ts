9c9508095dcf375e0775835f516a01c4fe62f063
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2024-10-31 13:30:08 +0100

    Updated registration from component to include other UI componenets like select, toggle-button and button component

--- libs/forms-lib/src/lib/registration-form/registration-form.component.ts
+++ libs/forms-lib/src/lib/registration-form/registration-form.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputComponent } from '@ng-mono/mui-lib'
+import { InputComponent } from '@ng-mono/mui-lib';
+import { DatePickerComponent } from '@ng-mono/mui-lib';
+import { SelectComponent } from '@ng-mono/mui-lib';
+import { SlideToggleComponent} from '@ng-mono/mui-lib';
+import { ButtonComponent, ButtonType} from '@ng-mono/mui-lib';
+import {ChipComponent} from '@ng-mono/mui-lib';
+
 
 @Component({
   selector: 'lib-registration-form',
   standalone: true,
   imports: [
     CommonModule,
-    InputComponent
+    InputComponent,
+    DatePickerComponent,
+    SelectComponent,
+    SlideToggleComponent,
+    ButtonComponent,
+    ChipComponent
   ],
   templateUrl: './registration-form.component.html',
   styleUrl: './registration-form.component.scss',
@@ -19,6 +30,17 @@ export class RegistrationFormComponent {
     email: string = '';
     phoneNumber: string = '';
     countryCode: string = '';
+    birthDate: Date | null = null;
+    educationLevel: string = '';
+    educationLevels: string[] = [
+      'High School',
+      'College',
+      'Bachelor\'s Degree',
+      'Master\'s Degree',
+      'PhD'
+    ];
+    agreeTerms: boolean = false;
+    programmingLanguages: string[] = [];
 
     onFirstNameChange(value: string): void {
       this.firstName = value;
@@ -35,8 +57,25 @@ export class RegistrationFormComponent {
     onPhoneNumberChange(value: string): void {
       this.phoneNumber = value;
     }
+
     onCountryCodeChange(value: string): void {
       this.countryCode = value;
+    }
+
+    onBirthDateChange(value: Date | null): void {
+      this.birthDate = value;
+    }
+
+    onEducationLevelChange(value: string): void {
+      this.educationLevel = value;
+    }
+
+    onAgreeTermsChange(value: boolean): void {
+      this.agreeTerms = value;
+    }
+
+    registerUser(): void {
+      console.log('Registering User'); 
     }
   
 }
