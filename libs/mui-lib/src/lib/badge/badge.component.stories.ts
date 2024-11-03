492f3b85045686b05f142c99cef9e3da37604a8e
sukrut29 <sukrutssathaye@gmail.com>
2024-11-03 16:32:19 +0530

    fully configure the badge component with various inputs. TODO: make different stories with different combinations of all inputs

--- libs/mui-lib/src/lib/badge/badge.component.stories.ts
+++ libs/mui-lib/src/lib/badge/badge.component.stories.ts
@@ -7,6 +7,35 @@ import { expect } from '@storybook/jest';
 const meta: Meta<BadgeComponent> = {
   component: BadgeComponent,
   title: 'BadgeComponent',
+  argTypes: {
+    size: {
+      options: [
+        'small',
+        'medium',
+        'large'
+      ],
+      control: 'radio'
+    },
+    content: {control: 'text'},
+    hostLabel: {control: 'text'},
+    disabled: {control: 'boolean'},
+    hidden: {control: 'boolean'},
+    overlap: {control: 'boolean'},
+    badgeFor: {
+      options: ['text', 'button', 'icon'],
+      control: 'radio'
+    },
+    position: {
+      options: [
+        'before',
+        'after',
+        'below',
+        'above',
+      ],
+      control: 'radio'
+    },
+    icon: {control: 'text'}
+  }
 };
 export default meta;
 type Story = StoryObj<BadgeComponent>;
--- libs/mui-lib/src/lib/badge/badge.component.ts
+++ libs/mui-lib/src/lib/badge/badge.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+
+export enum BadgeSize {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large'
+}
+
+export enum BadgePosition {
+  ABOVE = 'above',
+  AFTER = 'after',
+  BEFORE = 'before',
+  BELOW = 'below',
+  ABOVE_AFTER = 'above after',
+  ABOVE_BEFORE = 'above before',
+  BELOW_BEFORE = 'below before',
+  BELOW_AFTER = 'below after'
+
+}
+
+export enum BadgeFor {
+  BUTTON = 'button',
+  TEXT = 'text',
+  ICON = 'icon'
+}
 
 @Component({
   selector: 'lib-badge',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './badge.component.html',
   styleUrl: './badge.component.scss',
 })
 export class BadgeComponent {
-  
+
+  @Input() disabled: boolean = false;
+
+  @Input() hidden: boolean = false;
+
+  @Input() overlap: boolean = false;
+
+  @Input() content: string | number | undefined | null = 1;
+
+  @Input() size: BadgeSize = BadgeSize.MEDIUM
+
+  @Input() position: BadgePosition = BadgePosition.AFTER
+
+  @Input() badgeFor: BadgeFor = BadgeFor.TEXT;
+
+  @Input() hostLabel: string | undefined = 'I have a badge'
+
+  @Input() icon: string | undefined = 'home'
+
 }
