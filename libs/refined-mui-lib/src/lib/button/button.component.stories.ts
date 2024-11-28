dec6a335669d1d1a0db23d712aae42e68d0606d6
sukrut29 <sukrutssathaye@gmail.com>
2024-11-28 22:18:52 +0100

    add argTypes with defaultValues, types and description

--- libs/refined-mui-lib/src/lib/button/button.component.stories.ts
+++ libs/refined-mui-lib/src/lib/button/button.component.stories.ts
@@ -12,7 +12,84 @@ const meta: Meta<ButtonComponent> = {
 
   title: 'ButtonComponent',
   component: ButtonComponent,
-  title: 'ButtonComponent',
+  argTypes: {
+    label: {
+      table: {
+        defaultValue: { summary: 'Button Text' },
+        type: { summary: 'string' },
+      },
+      control: {
+        type: 'text',
+      },
+      description: 'The text to display on the button',
+    },
+    disabled: {
+      table: {
+        defaultValue: { summary: "False" },
+        type: { summary: 'boolean' },
+      },
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the button is disabled',
+    },
+    disabledInteractive: {
+      table: {
+        defaultValue: { summary: "False" },
+        type: { summary: 'boolean' },
+      },
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the button is interactive when disabled',
+    },
+    disableRipple: {
+      table: {
+        defaultValue: { summary: "False" },
+        type: { summary: 'boolean' },
+      },
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the button has a ripple effect',
+    },
+    buttonType: {
+      table: {
+        defaultValue: { summary: 'mat-raised-button' },
+        type: { summary: 'string' },
+      },
+      options: [
+        'mat-raised-button',
+        'mat-fab',
+        'mat-button',
+        'mat-flat-button',
+        'mat-stroked-button',
+        'mat-mini-fab',
+        'mat-icon-button'
+      ],
+      control: {
+        type: 'radio'
+      },
+      description: 'The type of button to display',
+    },
+    icon: {
+      table: {
+        defaultValue: { summary: 'home' },
+        type: { summary: 'string' },
+      },
+      control: {
+        type: 'text',
+      },
+      description: 'The icon to display on the button',
+    },
+    onClick: {
+      table: {
+        defaultValue: { summary: '() => {}' },
+        type: { summary: '() => void' },
+      },
+      description: 'The function to call when the button is clicked',
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<ButtonComponent>;
