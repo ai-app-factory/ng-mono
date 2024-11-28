e9ae5ce3ed19a4bd5cdb8f11777c19c321100262
sukrut29 <sukrutssathaye@gmail.com>
2024-11-28 22:19:54 +0100

    add stories with JSDoc comments to be displayed using autodocs

--- libs/refined-mui-lib/src/lib/button/button.component.stories.ts
+++ libs/refined-mui-lib/src/lib/button/button.component.stories.ts
@@ -99,6 +99,145 @@ export const Heading: Story = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/button works!/gi)).toBeTruthy();
+    expect(canvas.getByText("Button Text")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display a disabled button
+ * with args: `{ disabled: true }`
+ */
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Button Text")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display an icon button
+ * with args : `{
+ *    icon: 'home',
+ *    buttonType: 'mat-icon-button'
+ * }`
+ */
+export const Icon: Story = {
+  args: {
+    icon: 'home',
+    buttonType: 'mat-icon-button',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("home")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display a button with custom label
+ * with args: `{ label: 'this is the label text' }`
+ */
+export const CustomLabel: Story = {
+  args: {
+    label: 'this is the label text',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("this is the label text")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display a flat button
+ * with args: `{ buttonType: 'mat-flat-button' }`
+ */
+export const Flat: Story = {
+  args: {
+    buttonType: 'mat-flat-button',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Button Text")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display a stroked button
+ * with args: `{ buttonType: 'mat-stroked-button' }`
+ */
+export const Stroked: Story = {
+  args: {
+    buttonType: 'mat-stroked-button',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Button Text")).toBeTruthy();
+  },
+};
+
+/**
+ * Story to display a mini fab button
+ * with args: `{ buttonType: 'mat-mini-fab' }`
+ */
+
+export const MiniFab: Story = {
+  args: {
+    buttonType: 'mat-mini-fab',
+  },
+};
+
+/**
+ * Story to display a fab button
+ * with args: `{ buttonType: 'mat-fab' }`
+ */
+export const Fab: Story = {
+  args: {
+    buttonType: 'mat-fab',
+  },
+};
+
+/**
+ * Story to display a normal button
+ * with args: `{ buttonType: 'mat-button' }`
+ */
+export const Button: Story = {
+  args: {
+    buttonType: 'mat-button',
+  },
+};
+
+/**
+ * Story to display a raised button
+ * with args: `{ buttonType: 'mat-raised-button' }`
+ */
+export const Raised: Story = {
+  args: {
+    buttonType: 'mat-raised-button',
+  },
+};
+
+/**
+ * Story to display a disabled interactive button
+ * with args: `{
+ *    disabledInteractive: true,
+ *    disabled: true
+ * }`
+ */
+export const DisabledInteractive: Story = {
+  args: {
+    disabledInteractive: true,
+    disabled: true,
+  },
+};
+
+/**
+ * Story to display a button with disabled ripple effect
+ * with args: `{ disableRipple: true }`
+ */
+export const DisableRipple: Story = {
+  args: {
+    disableRipple: true,
   },
 };
