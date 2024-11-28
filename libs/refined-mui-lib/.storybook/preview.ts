051f4cc676e5dd422e706f03d1041209fb5f90ea
sukrut29 <sukrutssathaye@gmail.com>
2024-11-28 22:16:40 +0100

    import DocumentTemplate from its mdx file

--- libs/refined-mui-lib/.storybook/preview.ts
+++ libs/refined-mui-lib/.storybook/preview.ts
@@ -5,6 +5,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import DocumentationTemplate from './DocumentationTemplate.mdx';
 
 export const decorators = [
   moduleMetadata({
