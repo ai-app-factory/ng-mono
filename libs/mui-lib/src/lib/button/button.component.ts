90355ef4807ce402d22ee0748d1581941d7bf39a
sukrut29 <sukrutssathaye@gmail.com>
2024-11-28 15:35:53 +0100

    add JSDoc for ButtonType

--- libs/mui-lib/src/lib/button/button.component.ts
+++ libs/mui-lib/src/lib/button/button.component.ts
@@ -4,6 +4,19 @@ import { MatButton, MatButtonModule } from '@angular/material/button';
 import { FormsModule, FormControl } from '@angular/forms';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 
+/**
+ * Button Types
+ * @enum {string}
+ * @readonly
+ * @type ButtonType = 'mat-raised-button' | 'mat-fab' | 'mat-button' | 'mat-flat-button' | 'mat-stroked-button' | 'mat-mini-fab' | 'mat-icon-button'
+ * @field RAISED - mat-raised-button
+ * @field FAB - mat-fab
+ * @field BUTTON - mat-button
+ * @field FLAT - mat-flat-button
+ * @field STROKED - mat-stroked-button
+ * @field MINI_FAB - mat-mini-fab
+ * @field ICON - mat-icon-button
+ */
 export enum ButtonType {
   RAISED = 'mat-raised-button',
   FAB = 'mat-fab',
