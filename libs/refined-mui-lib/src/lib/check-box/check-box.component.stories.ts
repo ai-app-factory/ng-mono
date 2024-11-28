d76cbaa565040f79e39cdf94aa837c425ae0a275
sukrut29 <sukrutssathaye@gmail.com>
2024-11-29 00:46:26 +0100

    add refined check-box component capable of handling multiple tasks and subtasks with output for selection

--- libs/refined-mui-lib/src/lib/check-box/check-box.component.stories.ts
+++ libs/refined-mui-lib/src/lib/check-box/check-box.component.stories.ts
@@ -7,18 +7,311 @@ import { expect } from '@storybook/jest';
 const meta: Meta<CheckBoxComponent> = {
   component: CheckBoxComponent,
   title: 'CheckBoxComponent',
+  argTypes: {
+    checked: {
+      control: 'boolean',
+      description: 'Whether the checkbox is checked.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Whether the checkbox is disabled.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    labelPosition: {
+      options: ['before', 'after'],
+      control: { type: 'radio' },
+      description: 'Whether the label should appear after or before the checkbox.',
+      table: {
+        defaultValue: { summary: 'after' },
+        type: { summary: 'before | after' }
+      },
+    },
+    displayGroupLabel: {
+      control: 'boolean',
+      description: 'Whether the group label should be displayed.',
+      table: {
+        defaultValue: { summary: 'true' },
+        type: { summary: 'boolean' }
+      },
+    },
+    groupLabel: {
+      control: 'text',
+      description: 'The group label.',
+      table: {
+        defaultValue: { summary: 'To do list' },
+        type: { summary: 'string' }
+      },
+    },
+    disableRipple: {
+      control: 'boolean',
+      description: 'Whether the ripple effect should be disabled.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    disabledInteractive: {
+      control: 'boolean',
+      description: 'Whether the checkbox is disabled and not interactive.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    required: {
+      control: 'boolean',
+      description: 'Whether the checkbox is required.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    indeterminate: {
+      control: 'boolean',
+      description: 'Whether the checkbox is indeterminate.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<CheckBoxComponent>;
 
-export const Primary: Story = {
-  args: {},
-};
 
 export const Heading: Story = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/check-box works!/gi)).toBeTruthy();
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const Checked: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: true
+    }],
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const Unchecked: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    disabled: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const CheckedDisabled: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: true
+    }],
+    disabled: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const Indeterminate: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: true }
+      ]
+
+    }],
+
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const IndeterminateDisabled: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: true }
+      ]
+
+    }],
+    disabled: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const Required: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    required: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const DisabledRipple: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    disableRipple: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const DisabledInteractive: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    disabledInteractive: true,
+    disabled: true
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const CustomGroupLabel: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    groupLabel: 'Group Label'
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Group Label")).toBeTruthy();
+  },
+};
+
+export const NoGroupLabel: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    displayGroupLabel: false
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.queryByText("To do list")).toBeNull();
+  },
+};
+
+export const BeforeLabel: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: true }
+      ]
+    }],
+    labelPosition: 'before'
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
+  },
+};
+
+export const HideSelected: Story = {
+  args: {
+
+    tasks: [{
+      name: 'Option 1',
+      completed: false
+    }],
+    showSelected: false
+
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText("Option 1")).toBeTruthy();
   },
 };
--- libs/refined-mui-lib/src/lib/check-box/check-box.component.ts
+++ libs/refined-mui-lib/src/lib/check-box/check-box.component.ts
@@ -1,11 +1,133 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  computed,
+  signal,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
 
 @Component({
   selector: 'lib-check-box',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    FormsModule
+  ],
   templateUrl: './check-box.component.html',
   styleUrl: './check-box.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckBoxComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckBoxComponent {}
+export class CheckBoxComponent implements ControlValueAccessor {
+
+
+
+  @Input() displayGroupLabel: boolean = true;
+  @Input() groupLabel: string = 'To do list';
+  @Input() checked: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() labelPosition: 'before' | 'after' = 'after';
+  // @Input() checkboxAction: 'noop' | 'check' | 'check-indeterminate' | undefined;
+  @Input() disableRipple: boolean = false;
+  @Input() disabledInteractive: boolean = false;
+  @Input() required: boolean = false;
+  @Input() indeterminate: boolean = false;
+  @Input() showSelected: boolean = true;
+
+  @Input() set tasks(tasks: Task[]) {
+    this._tasks.set(tasks);
+  }
+
+  protected _tasks = signal<Task[]>([
+    {
+      name: 'Option 1',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: false },
+        { name: 'Subtask 3', completed: true }
+      ]
+    },
+    {
+      name: 'Parent Task 2',
+      completed: false,
+      subtasks: [
+        { name: 'Subtask 1', completed: false },
+        { name: 'Subtask 2', completed: true }
+      ]
+    }
+  ]);
+
+  @Output() taskChange = new EventEmitter<Task>();
+  @Output() selectionChange = new EventEmitter<Task[]>();
+
+  readonly partiallyComplete = (index: number) => {
+    const task = this._tasks()[index];
+    return task.subtasks?.some(subtask => subtask.completed) &&
+      !task.subtasks.every(subtask => subtask.completed);
+  };
+
+  onCheckboxChange(event: MatCheckboxChange): void {
+    const task = this._tasks()[0]; // Assuming you want to emit the first task
+    this.taskChange.emit({
+      ...task,
+      completed: event.checked
+    });
+  }
+
+  // ControlValueAccessor implementation
+  private onChange: (value: Task[]) => void = () => {};
+
+  registerOnTouched(fn: any): void {
+  }
+
+
+
+  writeValue(value: Task[]): void {
+    this._tasks.set(value);
+  }
+
+  registerOnChange(fn: (value: Task[]) => void): void {
+    this.onChange = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  update(completed: boolean, index: number, subtaskIndex?: number) {
+    this._tasks.update(task => {
+      if (subtaskIndex === undefined) {
+        task[index].completed = completed;
+        task[index].subtasks?.forEach(subtask => subtask.completed = completed);
+      } else {
+
+        task[index].subtasks![subtaskIndex].completed = completed;
+        task[index].completed = task[index].subtasks?.every(subtask => subtask.completed) ?? true;
+      }
+      this.onChange([...task]);
+      this.selectionChange.emit([...task]);
+      return [...task];
+    });
+  }
+
+}
