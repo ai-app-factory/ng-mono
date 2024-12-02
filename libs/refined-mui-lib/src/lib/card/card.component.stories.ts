8e95c665f64c73f72acc5d230ac80801dd27153d
sukrut29 <sukrutssathaye@gmail.com>
2024-12-02 23:54:59 +0100

    add initial refined card component

--- libs/refined-mui-lib/src/lib/card/card.component.stories.ts
+++ libs/refined-mui-lib/src/lib/card/card.component.stories.ts
@@ -7,6 +7,83 @@ import { expect } from '@storybook/jest';
 const meta: Meta<CardComponent> = {
   component: CardComponent,
   title: 'CardComponent',
+  argTypes: {
+    title: {
+      control: 'text',
+      description: 'The title of the card',
+      table: {
+        defaultValue: { summary: 'Card Title' },
+        type: { summary: 'string' },
+      }
+    },
+    subTitle: {
+      control: 'text',
+      description: 'The subtitle of the card',
+      table: {
+        defaultValue: { summary: 'Card Subtitle' },
+        type: { summary: 'string' },
+      }
+    },
+    content: {
+      control: 'text',
+      description: 'The content of the card',
+      table: {
+        defaultValue: { summary: 'Card Content' },
+        type: { summary: 'string' },
+      }
+    },
+    cardImage: {
+      control: 'text',
+      description: 'The image of the card',
+      table: {
+        defaultValue: { summary: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },
+        type: { summary: 'string' },
+      }
+    },
+    cardAppearance: {
+      options: ['outlined', 'raised'],
+      control: { type: 'radio' },
+      description: 'The appearance of the card',
+      table: {
+        defaultValue: { summary: 'raised' },
+        type: { summary: 'outlined | raised' },
+      }
+    },
+    imageSize: {
+      options: ['small', 'medium', 'large', 'extra-large'],
+      control: { type: 'radio' },
+      description: 'The size of the image',
+      table: {
+        defaultValue: { summary: 'medium' },
+        type: { summary: 'small | medium | large | extra-large' },
+      }
+    },
+    actions: {
+      control: 'object',
+      description: 'The actions of the card',
+      table: {
+        defaultValue: { summary: 'Action 1, Action 2' },
+        type: { summary: 'ButtonComponent[]' },
+      }
+    },
+    chips: {
+      control: 'object',
+      description: 'The chips of the card',
+      table: {
+        defaultValue: { summary: 'Chip 1, Chip 2' },
+        type: { summary: 'ChipComponent[]' },
+      }
+    },
+    cardType: {
+      options: ['default', 'with-chips', 'with-buttons'],
+      control: { type: 'radio' },
+      description: 'The type of the card',
+      table: {
+        defaultValue: { summary: 'default' },
+        type: { summary: 'default | with-chips | with-buttons' },
+      }
+    }
+  },
 };
 export default meta;
 type Story = StoryObj<CardComponent>;
@@ -21,4 +98,59 @@ export const Heading: Story = {
     const canvas = within(canvasElement);
     expect(canvas.getByText(/card works!/gi)).toBeTruthy();
   },
+};
+
+export const CustomTitle: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: ""
+  }
+};
+
+export const CustomSubTitle: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed"
+  }
+};
+
+export const ImageSizeSmall: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed",
+    imageSize: "small"
+  }
+};
+
+export const ImageSizeMedium: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed",
+    imageSize: "medium"
+  }
+};
+
+export const ImageSizeLarge: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed",
+    imageSize: "large"
+  }
+};
+
+export const ImageSizeExtraLarge: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed",
+    imageSize: "extra-large"
+  }
+};
+
+export const CustomContent: Story = {
+  args: {
+    title: "Shiba Inu",
+    subTitle: "Is my favorite dog breed",
+    imageSize: "extra-large",
+    content: "The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting."
+  }
 };
--- libs/refined-mui-lib/src/lib/card/card.component.ts
+++ libs/refined-mui-lib/src/lib/card/card.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import { ButtonComponent } from '../button/button.component';
+import { Chip, ChipComponent } from '../chip/chip.component';
+
 
 @Component({
   selector: 'lib-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    ChipComponent,
+    MatCardModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardComponent {}
+export class CardComponent {
+  @Input() title?: string = 'Card Title';
+  @Input() subTitle?: string = 'Card Subtitle';
+  @Input() content?: string = 'Card Content';
+  @Input() cardImage?: string = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
+
+  @Input() cardAppearance?:  'outlined' | 'raised'  = 'raised';
+
+  @Input() imageSize?: 'small' | 'medium' | 'large' | 'extra-large' = 'medium';
+
+  @Input() actions?: ButtonComponent[] = [
+    {
+      label: 'Action 1', buttonType: 'mat-raised-button', disabled: false, disabledInteractive: false, disableRipple: false,
+      icon: null,
+      onClick: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      buttonClick: new EventEmitter<any>(),
+      onButtonPress: function (): void {
+        throw new Error('Function not implemented.');
+      }
+    },
+    {
+      label: 'Action 2', buttonType: 'mat-raised-button', disabled: false, disabledInteractive: false, disableRipple: false,
+      icon: null,
+      onClick: function (): void {
+        throw new Error('Function not implemented.');
+      },
+      buttonClick: new EventEmitter<any>(),
+      onButtonPress: function (): void {
+        throw new Error('Function not implemented.');
+      }
+    }
+
+  ];
+  @Input() chips?: ChipComponent[] = [
+    new ChipComponent(),
+    new ChipComponent()
+  ];
+
+
+  @Input() cardType?: 'default' | 'with-chips' | 'with-buttons' = 'default';
+}
