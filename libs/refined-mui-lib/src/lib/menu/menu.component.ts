109d45a8df60b2207b3367945df40209b918f1f8
sukrut29 <sukrutssathaye@gmail.com>
2024-12-05 18:05:55 +0100

    add import

--- libs/refined-mui-lib/src/lib/menu/menu.component.ts
+++ libs/refined-mui-lib/src/lib/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
