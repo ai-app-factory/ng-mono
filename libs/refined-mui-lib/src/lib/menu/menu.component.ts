0d829fca93658cfad8e85ad491bd55f8074c9601
sukrut29 <sukrutssathaye@gmail.com>
2024-12-05 18:06:26 +0100

    add interface Menu and additional imports

--- libs/refined-mui-lib/src/lib/menu/menu.component.ts
+++ libs/refined-mui-lib/src/lib/menu/menu.component.ts
@@ -1,5 +1,20 @@
 import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+export interface Menu {
+  title: string;
+  icon?: string;
+  link?: string;
+  disabled?: boolean;
+  role?: 'menuitem' | 'menuitemradio' | 'menuitemcheckbox';
+  disabledRipple?: boolean;
+  children?: Menu[];
+}
+
+export type MenuCloseReason = void | 'click' | 'keydown' | 'tab';
 
 @Component({
   selector: 'lib-menu',
