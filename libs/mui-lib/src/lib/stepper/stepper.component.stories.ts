943378d9e6a1c0bd830ec16e4df601642d2641d0
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2024-12-19 12:17:44 +0100

    Add Stepper component to mui-lib

---
+++ libs/mui-lib/src/lib/stepper/stepper.component.stories.ts
@@ -0,0 +1,102 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { StepperComponent } from './stepper.component';
+
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { table } from 'console';
+
+const meta: Meta<StepperComponent> = {
+  component: StepperComponent,
+  title: 'StepperComponent',
+  argTypes: {
+    labelPosition: {
+      control: 'radio',
+      options: ['bottom', 'end'],
+      description: 'The position of the label',
+      table: {
+        type: { summary: 'bottom | end' },
+        defaultValue: { summary: 'end' },
+      },
+    },
+    headerPosition: {
+      control: 'radio',
+      options: ['top', 'bottom'],
+      description: 'The position of the header',
+      table: {
+        type: { summary: 'top | bottom' },
+        defaultValue: { summary: 'top' },
+      },
+    },
+    editable: {
+      control: 'boolean',
+      description: 'Editable',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+    orientation: {
+      control: 'radio',
+      options: ['vertical', 'horizontal'],
+      description: 'The orientation of the stepper',
+      table: {
+        type: { summary: 'vertical | horizontal' },
+        defaultValue: { summary: 'horizontal' },
+      },
+    },
+    linear: {
+      control: 'boolean',
+      description: 'Linear stepper',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+    disableRipple: {
+      control: 'boolean',
+      description: 'Disable ripple effect',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    questions: {
+      description: 'Questions for the stepper',
+      table: {
+        type: { summary: 'Question[]' },
+        defaultValue: { summary: '[]' },
+      },
+      control: {
+        type: 'object',
+      },
+      properties: {
+        label: {
+          control: 'text'
+        },
+        placeholder: {
+          control: 'text'
+        },
+        required: {
+          control: 'boolean'
+        },
+        errorMessage: {
+          control: 'text'
+        }
+      },
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<StepperComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/stepper works!/gi)).toBeTruthy();
+  },
+};
---
+++ libs/mui-lib/src/lib/stepper/stepper.component.ts
@@ -0,0 +1,97 @@
+import { 
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule, FormControl, AbstractControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+
+export interface Question {
+  label: string;
+  placeholder: string;
+  formControl: FormControl;
+  required?: boolean;
+  errorMessage: string;
+}
+
+@Component({
+  selector: 'lib-stepper',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+
+export class StepperComponent {
+  @Input() orientation: 'vertical' | 'horizontal' = 'horizontal';
+  @Input() linear: boolean = true;
+  @Input() disableRipple: boolean = false;
+  @Input() editable: boolean = true;
+  @Input() labelPosition: 'bottom' | 'end' = 'end';
+  @Input() headerPosition: 'top' | 'bottom' = 'top';
+
+  @Input() set questions(questions: Question[]) {
+    this._questions.set(questions);
+  }
+
+  protected _questions = signal<Question[]>([
+    {
+      label: 'What is your name?',
+      placeholder: 'Name',
+      formControl: new FormControl('', [Validators.required]),
+      required: false,
+      errorMessage: 'Name is required',
+    },
+    {
+      label: 'What is your age?',
+      placeholder: 'Age',
+      formControl: new FormControl('', [Validators.required]),
+      required: true,
+      errorMessage: 'Age is required',
+    },
+    {
+      label: 'What is your favorite color?',
+      placeholder: 'Color',
+      formControl: new FormControl('', [Validators.required]),
+      required: true,
+      errorMessage: 'Color is required',
+    },
+  ]);
+
+  @Output() stepChange = new EventEmitter<number>();
+  @Output() completionChange = new EventEmitter<Question[]>();
+
+  constructor(private _formBuilder: FormBuilder) {}
+  
+  onStepChange(index: number): void {
+    const currentQuestion = this._questions()[index];
+    this.stepChange.emit(index);
+  }
+
+  onCompletion(): void {
+    this.completionChange.emit(this._questions());
+  }
+}
