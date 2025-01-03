4f713b18989fdf6139985004828639a25004bdf6
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-01-03 10:38:10 +0100

    Chromatic Errors: Remove the play function for the basic toolbar component

--- libs/mui-lib/src/lib/basic-toolbar/basic-toolbar.component.stories.ts
+++ libs/mui-lib/src/lib/basic-toolbar/basic-toolbar.component.stories.ts
@@ -17,8 +17,4 @@ export const Primary: Story = {
 
 export const Heading: Story = {
   args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/basic-toolbar works!/gi)).toBeTruthy();
-  },
 };
