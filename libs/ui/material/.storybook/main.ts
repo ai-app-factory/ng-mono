056611818a606f95e3ea795692b2cda96e02d830
sukrut29 <sukrutssathaye@gmail.com>
2025-02-07 23:26:38 +0100

    enable auto-docs addon

--- libs/ui/material/.storybook/main.ts
+++ libs/ui/material/.storybook/main.ts
@@ -2,11 +2,19 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
+  ],
   framework: {
     name: '@storybook/angular',
     options: {},
   },
+  docs: {
+    autodocs: true,
+    defaultName: 'Docs'
+  }
 };
 
 export default config;
