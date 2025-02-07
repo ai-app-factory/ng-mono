2ffdcb443a4ff9e0a2f3069ea0829e88163eb370
sukrut29 <sukrutssathaye@gmail.com>
2025-02-07 23:27:54 +0100

    set up preview with documentation template, browser animation and default inputs like date and background color

---
+++ libs/ui/material/.storybook/preview.ts
@@ -0,0 +1,45 @@
+import { moduleMetadata, Preview } from '@storybook/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import DocumentationTemplate from './DocumentationTemplate.mdx';
+
+export const decorators = [
+  moduleMetadata({
+    imports: [
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatDatepickerModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatIconModule,
+    ],
+  }),
+];
+
+// Set up compdoc
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from "../docs/documentation.json";
+setCompodocJson(docJson);
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      }
+    },
+    docs: {
+      toc: true,
+      page: DocumentationTemplate,
+    },
+
+  },
+};
+
+export default preview;
