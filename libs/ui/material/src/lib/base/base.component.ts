cfca9ec545a222b14651eddcb975147eceb49f83
sukrut29 <sukrutssathaye@gmail.com>
2025-02-20 17:01:03 +0100

    remove getStyles and getTextColor functions from BaseComponent

--- libs/ui/material/src/lib/base/base.component.ts
+++ libs/ui/material/src/lib/base/base.component.ts
@@ -20,22 +20,4 @@ export class BaseComponent {
     return baseClasses;
   }
 
-  get styles() {
-    return {
-      backgroundColor: this.color,
-      color: this.getTextColor(this.color),
-      padding: `${this.size / 2}px ${this.size}px`,
-      fontSize: `${this.size}px`
-    };
-  }
-
-  private getTextColor(backgroundColor: string): string {
-    const color = backgroundColor.substring(1);
-    const rgb = parseInt(color, 16);
-    const r = (rgb >> 16) & 0xff;
-    const g = (rgb >>  8) & 0xff;
-    const b = (rgb >>  0) & 0xff;
-    const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-    return brightness > 125 ? '#000000' : '#ffffff';
-  }
 }
