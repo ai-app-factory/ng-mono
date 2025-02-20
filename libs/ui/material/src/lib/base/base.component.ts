de33dafa1eea31fbfb9a1017928f785dc42e689c
sukrut29 <sukrutssathaye@gmail.com>
2025-02-20 17:01:22 +0100

    remove color input

--- libs/ui/material/src/lib/base/base.component.ts
+++ libs/ui/material/src/lib/base/base.component.ts
@@ -8,7 +8,6 @@ import { Component, Input } from '@angular/core';
 export class BaseComponent {
   @Input() typography: 'heading1' | 'heading2' | 'paragraph' = 'paragraph';
   @Input() shape: 'rounded' | 'square' | 'pill' = 'rounded';
-  @Input() color: string = '#6200ea';
   @Input() size: number = 14;
   @Input() disabled?: boolean = false;
 
