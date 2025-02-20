b19f8c18bb5a97a3bba69bb9d4c6f1d52c507dfb
sukrut29 <sukrutssathaye@gmail.com>
2025-02-20 16:56:26 +0100

    move imports to top and add globalTypes and withTheme for adding and selecting themes from storybook

--- libs/ui/material/.storybook/preview.ts
+++ libs/ui/material/.storybook/preview.ts
@@ -6,6 +6,24 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import DocumentationTemplate from './DocumentationTemplate.mdx';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from "../docs/documentation.json";
+
+// Set up compdoc
+setCompodocJson(docJson);
+
+export const withTheme = (
+  Story: any,
+  context: any,
+): any => {
+  const selectedTheme = context.globals.theme;
+  const body = document.body;
+  body.className = ''; // Clear existing classes
+  body.classList.add(selectedTheme);
+  // Apply the selected theme using the service
+
+  return Story();
+}
 
 export const decorators = [
   moduleMetadata({
@@ -18,12 +36,29 @@ export const decorators = [
       MatIconModule,
     ],
   }),
+  withTheme
 ];
 
-// Set up compdoc
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from "../docs/documentation.json";
-setCompodocJson(docJson);
+
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'indigo-pink',
+    toolbar: {
+      icon: 'paintbrush',
+      items: [
+        { value: 'dark-theme', right: '🌙', title: 'Dark Theme' },
+        { value: 'light-theme', right: '☀️', title: 'Light Theme' },
+        { value: 'rose-red', right: '🌹', title: 'Rose Red Theme' },
+        {value: 'cyan-orange', right: '🟦', title: 'Cyan Orange Theme'},
+      ],
+      showName: true,
+    },
+  },
+};
+
 
 const preview: Preview = {
   parameters: {
