846a578d8fa903f7bd8326ea24d46b37c97a663a
sukrut29 <sukrutssathaye@gmail.com>
2025-02-20 18:27:29 +0100

    add exports

--- libs/ui/material/src/index.ts
+++ libs/ui/material/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/button/button.component';
+export * from './lib/check-box/check-box.component';
+export * from './lib/button-toggle/button-toggle.component';
+export * from './lib/base/base.component';
+
