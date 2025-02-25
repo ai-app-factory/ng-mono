87253e0606a49f449baed63c2035dc62808eb351
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 13:21:45 +0100

    gen tool-bar comp

---
+++ libs/ui/material/src/lib/tool-bar/tool-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tool-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tool-bar.component.html',
+  styleUrl: './tool-bar.component.scss',
+})
+export class ToolBarComponent {}
