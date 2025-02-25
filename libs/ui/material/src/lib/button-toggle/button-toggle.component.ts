2f285052dc035bffcabcb5cf14665f723ce96e7c
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 11:51:37 +0100

    generate button-toggle component using angular component gen

---
+++ libs/ui/material/src/lib/button-toggle/button-toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-button-toggle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button-toggle.component.html',
+  styleUrl: './button-toggle.component.scss',
+})
+export class ButtonToggleComponent {}
