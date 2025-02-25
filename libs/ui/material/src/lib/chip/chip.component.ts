dbe67f07358d2df4375bbed29f3414251fba3c88
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 17:04:14 +0100

    add imports and create Chip interface

--- libs/ui/material/src/lib/chip/chip.component.ts
+++ libs/ui/material/src/lib/chip/chip.component.ts
@@ -1,6 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  MatChipsModule,
+  MatChipInputEvent,
+  MatChipEditedEvent,
+  MatChipSelectionChange
+} from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray
+} from '@angular/cdk/drag-drop';
 
+
+export interface Chip {
+  label: string;
+  removable?: boolean;
+  withAvatar?: boolean;
+  avatar?: string;
+  avatarAlt?: string;
+  disabled?: boolean;
+  selectable?: boolean;
+  editable?: boolean;
+
+}
 @Component({
   selector: 'lib-chip',
   standalone: true,
