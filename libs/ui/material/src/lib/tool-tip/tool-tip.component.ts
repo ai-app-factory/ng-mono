809b2bdc4cf21d0d82112d7a21a4a079e92e21c3
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 13:21:57 +0100

    gen tool-tip component

---
+++ libs/ui/material/src/lib/tool-tip/tool-tip.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tool-tip',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tool-tip.component.html',
+  styleUrl: './tool-tip.component.scss',
+})
+export class ToolTipComponent {}
