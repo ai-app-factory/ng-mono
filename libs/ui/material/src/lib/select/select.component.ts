410a9aa18a91f899a7e8df916a9afae9672e2b9a
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 13:20:04 +0100

    gen select comp

---
+++ libs/ui/material/src/lib/select/select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-select',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class SelectComponent {}
