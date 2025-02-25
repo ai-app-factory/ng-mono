fd59c0a746ef318ce3ed8725d9b97f0f48ee7de5
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 13:20:44 +0100

    gen snack-bar comp

---
+++ libs/ui/material/src/lib/snack-bar/snack-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-snack-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './snack-bar.component.html',
+  styleUrl: './snack-bar.component.scss',
+})
+export class SnackBarComponent {}
