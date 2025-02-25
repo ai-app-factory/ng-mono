2679a00a8148fd1d504528b133effaa7e95d011d
sukrut29 <sukrutssathaye@gmail.com>
2025-02-25 13:20:57 +0100

    gen stepper component

---
+++ libs/ui/material/src/lib/stepper/stepper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-stepper',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.scss',
+})
+export class StepperComponent {}
