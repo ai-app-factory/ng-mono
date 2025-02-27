693c26c110f57958ba633a569f8b069c55087da1
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-02-27 20:39:56 +0100

    Add table component, Error with 'import { PeriodicElement, ELEMENT_DATA } from './models/element-data';'

--- libs/ui/material/src/lib/table/table.component.ts
+++ libs/ui/material/src/lib/table/table.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, Input  } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { PeriodicElement, ELEMENT_DATA } from './models/element-data';
 
 @Component({
   selector: 'lib-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent {}
+export class TableComponent implements AfterViewInit {
+
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+
+}
