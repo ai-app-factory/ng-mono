6a6c1d84b9391bcd401346d429a55817eafe3362
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:46:27 +0100

    Generate tool-bar component using nx angular

---
+++ libs/ui/mui-latest/src/lib/tool-bar/tool-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tool-bar',
+  imports: [CommonModule],
+  templateUrl: './tool-bar.component.html',
+  styleUrl: './tool-bar.component.scss',
+})
+export class ToolBarComponent {}
