f372e9311be5aa635633e9f3f1573843a3a50380
sukrut29 <sukrutssathaye@gmail.com>
2025-03-12 12:09:49 +0100

    generate bottom-sheet comp using nx angular

--- libs/ui/mui-latest/src/index.ts
+++ libs/ui/mui-latest/src/index.ts
@@ -5,3 +5,6 @@ export * from './lib/button/button.component';
 export * from './lib/stepper/stepper.component';
 
 export * from './lib/table/table.component';
+
+export * from './lib/bottom-sheet/bottom-sheet.component';
+
