53b5d133b5c43b6caa9547646ee1ecd664140d7d
sukrut29 <sukrutssathaye@gmail.com>
2025-03-12 17:46:45 +0100

    define base component for styling

--- libs/ui/mui-latest/src/lib/base/base.component.ts
+++ libs/ui/mui-latest/src/lib/base/base.component.ts
@@ -7,4 +7,24 @@ import { CommonModule } from '@angular/common';
   template: '',
   styleUrl: './base.component.scss',
 })
-export class BaseComponent {}
+export class BaseComponent {
+  @Input() typography: 'h1' | 'h2' | 'h3' | 'h4' | 'p' = 'p';
+  @Input() shape: 'square' | 'rounded' | 'pill' = 'rounded';
+  @Input() size: number = 16;
+  @Input() borderRadius?: number = 4;
+
+  get classes() {
+    return [
+      this.typography,
+      this.shape,
+      'custom-size'
+    ]
+  }
+
+  get styles() {
+    return {
+      fontSize: `${this.size}px`,
+      padding: `${this.size / 2}px ${this.size}px`
+    }
+  }
+}
