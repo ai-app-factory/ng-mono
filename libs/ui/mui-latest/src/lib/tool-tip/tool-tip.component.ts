0e1480aa947946c9eb443465475651f7111bb299
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:46:55 +0100

    Generate tool-tip component using nx angular

---
+++ libs/ui/mui-latest/src/lib/tool-tip/tool-tip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tool-tip',
+  imports: [CommonModule],
+  templateUrl: './tool-tip.component.html',
+  styleUrl: './tool-tip.component.scss',
+})
+export class ToolTipComponent {}
