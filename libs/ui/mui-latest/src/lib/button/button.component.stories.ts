481fe4bd2581b074907f00967fe1fb7a8b8a4e73
sukrut29 <sukrutssathaye@gmail.com>
2025-03-12 18:26:34 +0100

    add docs for button

--- libs/ui/mui-latest/src/lib/button/button.component.stories.ts
+++ libs/ui/mui-latest/src/lib/button/button.component.stories.ts
@@ -2,7 +2,12 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
-
+import { baseComponentArgs } from '../base/base.component.args';
+/**
+ * `ButtonComponent` is an enhanced mui-button component
+ * that can be used in Angular applications.
+ * Use `buttonType` to select the different types of buttons.
+ */
 const meta: Meta<ButtonComponent> = {
   component: ButtonComponent,
   title: 'ButtonComponent',
