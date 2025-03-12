9a8144a7f74c0d9e57eddd3968aa1023de5cb971
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:45:53 +0100

    Generate time-picker component using nx angular

---
+++ libs/ui/mui-latest/src/lib/time-picker/time-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-time-picker',
+  imports: [CommonModule],
+  templateUrl: './time-picker.component.html',
+  styleUrl: './time-picker.component.scss',
+})
+export class TimePickerComponent {}
