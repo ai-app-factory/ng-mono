cfeb2228a2e78c4dc9d532589b6205c6719a3984
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:43:57 +0100

    Generate slider component using nx angular

---
+++ libs/ui/mui-latest/src/lib/slider/slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-slider',
+  imports: [CommonModule],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.scss',
+})
+export class SliderComponent {}
