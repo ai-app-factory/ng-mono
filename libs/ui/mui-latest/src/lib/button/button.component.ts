f8059e7913fab1831b0ed5afde2dc00b56630728
sukrut29 <sukrutssathaye@gmail.com>
2025-03-12 18:28:17 +0100

    add imports for button comp

--- libs/ui/mui-latest/src/lib/button/button.component.ts
+++ libs/ui/mui-latest/src/lib/button/button.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  MatButton,
+  MatButtonModule,
+} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BaseComponent } from '../base/base.component';
 @Component({
   selector: 'lib-button',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButton
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
