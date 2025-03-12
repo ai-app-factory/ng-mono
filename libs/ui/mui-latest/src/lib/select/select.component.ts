81df80505b20b197366b74d457c6d7d475be8b54
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:42:50 +0100

    Generate select component using nx angular

---
+++ libs/ui/mui-latest/src/lib/select/select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-select',
+  imports: [CommonModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+})
+export class SelectComponent {}
