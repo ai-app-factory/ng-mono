276fdc56d3e5ae29f965a8445dbb8e42105f6bb6
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:50:11 +0100

    Update index.ts to export generated components

--- libs/ui/mui-latest/src/index.ts
+++ libs/ui/mui-latest/src/index.ts
@@ -9,3 +9,43 @@ export * from './lib/table/table.component';
 export * from './lib/bottom-sheet/bottom-sheet.component';
 
 export * from './lib/button-toggle/button-toggle.component';
+
+export * from './lib/card/card.component';
+
+export * from './lib/check-box/check-box.component';
+
+export * from './lib/chip/chip.component';
+
+export * from './lib/date-picker/date-picker.component';
+
+export * from './lib/dialog/dialog.component';
+
+export * from './lib/input/input.component';
+
+export * from './lib/list/list.component';
+
+export * from './lib/menu/menu.component';
+
+export * from './lib/progress-indicator/progress-indicator.component';
+
+export * from './lib/radio-button/radio-button.component';
+
+export * from './lib/select/select.component';
+
+export * from './lib/slide-toggle/slide-toggle.component';
+
+export * from './lib/slider/slider.component';
+
+export * from './lib/snack-bar/snack-bar.component';
+
+export * from './lib/tabs/tabs.component';
+
+export * from './lib/time-picker/time-picker.component';
+
+export * from './lib/tool-bar/tool-bar.component';
+
+export * from './lib/tool-tip/tool-tip.component';
+
+export * from './lib/tree/tree.component';
+
+export * from './lib/badge/badge.component';
