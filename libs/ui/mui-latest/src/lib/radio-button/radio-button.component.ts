70c33e3b963c00ed82a10b15c51ecb34aa872869
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:42:25 +0100

    Generate radio-button component using nx angular

---
+++ libs/ui/mui-latest/src/lib/radio-button/radio-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-radio-button',
+  imports: [CommonModule],
+  templateUrl: './radio-button.component.html',
+  styleUrl: './radio-button.component.scss',
+})
+export class RadioButtonComponent {}
