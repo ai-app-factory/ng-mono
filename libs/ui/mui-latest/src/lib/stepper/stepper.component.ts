f770c14e675341f8d9c9c562350a81afcc553377
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 15:51:13 +0100

    Update stepper component and its stories

--- libs/ui/mui-latest/src/lib/stepper/stepper.component.ts
+++ libs/ui/mui-latest/src/lib/stepper/stepper.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule, FormControl, AbstractControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface Step {
+  label: string;
+  placeholder: string;
+  formControl: FormGroup;
+  required?: boolean;
+  errorMessage: string;
+}
 
 @Component({
   selector: 'lib-stepper',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true },
+    },
+  ],
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StepperComponent {}
+
+
+export class StepperComponent {
+  @Input() orientation: 'vertical' | 'horizontal' = 'horizontal';
+  @Input() linear: boolean = true;
+  @Input() disableRipple: boolean = false;
+  @Input() editable: boolean = true;
+  @Input() labelPosition: 'bottom' | 'end' = 'end';
+  @Input() headerPosition: 'top' | 'bottom' = 'top';
+
+  @Input() set steps(steps: Step[]) {
+    this._steps.set(steps);
+  }
+
+  protected _steps = signal<Step[]>([
+    {
+      label: 'What is your name?',
+      placeholder: 'Name',
+      formControl: new FormGroup('', [Validators.required]),
+      required: false,
+      errorMessage: 'Name is required',
+    },
+    {
+      label: 'What is your age?',
+      placeholder: 'Age',
+      formControl: new FormGroup('', [Validators.required]),
+      required: true,
+      errorMessage: 'Age is required',
+    },
+    {
+      label: 'What is your favorite color?',
+      placeholder: 'Color',
+      formControl: new FormGroup('', [Validators.required]),
+      required: true,
+      errorMessage: 'Color is required',
+    },
+  ]);
+
+  @Output() stepChange = new EventEmitter<number>();
+  @Output() completionChange = new EventEmitter<Step[]>();
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  onStepChange(index: number): void {
+    const currentQuestion = this._steps()[index];
+    this.stepChange.emit(index);
+  }
+
+  onCompletion(): void {
+    this.completionChange.emit(this._steps());
+  }
+}
