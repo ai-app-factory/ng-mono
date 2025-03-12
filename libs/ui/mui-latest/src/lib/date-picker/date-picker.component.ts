132645d8ff280bed597e794e3ac5c1d5e4090395
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:38:22 +0100

    Generate date-picker component using nx angular

---
+++ libs/ui/mui-latest/src/lib/date-picker/date-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-date-picker',
+  imports: [CommonModule],
+  templateUrl: './date-picker.component.html',
+  styleUrl: './date-picker.component.scss',
+})
+export class DatePickerComponent {}
