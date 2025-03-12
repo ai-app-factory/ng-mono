28fb9c6b77221997dacfbc4b2fb381465ccf13b0
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:41:51 +0100

    Generate progress-indicator component using nx angular

---
+++ libs/ui/mui-latest/src/lib/progress-indicator/progress-indicator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-progress-indicator',
+  imports: [CommonModule],
+  templateUrl: './progress-indicator.component.html',
+  styleUrl: './progress-indicator.component.scss',
+})
+export class ProgressIndicatorComponent {}
