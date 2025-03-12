426edb7a62ecbd6f8b6e41d5ebaa6c373d1215b6
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 11:53:16 +0100

    Add stepper component to mui-latest

---
+++ libs/ui/mui-latest/src/lib/stepper/stepper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-stepper',
+  imports: [CommonModule],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.scss',
+})
+export class StepperComponent {}
