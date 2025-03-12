cde2899080d2b900c2169e8aee0a4813df2d46fa
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:44:39 +0100

    Generate snack-bar component using nx angular

---
+++ libs/ui/mui-latest/src/lib/snack-bar/snack-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-snack-bar',
+  imports: [CommonModule],
+  templateUrl: './snack-bar.component.html',
+  styleUrl: './snack-bar.component.scss',
+})
+export class SnackBarComponent {}
