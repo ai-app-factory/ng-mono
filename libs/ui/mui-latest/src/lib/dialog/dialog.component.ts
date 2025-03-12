8e3cff7854ec909918f51a4323843dcb08fcfc56
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 13:38:50 +0100

    Generate dialog component using nx angular

---
+++ libs/ui/mui-latest/src/lib/dialog/dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-dialog',
+  imports: [CommonModule],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss',
+})
+export class DialogComponent {}
