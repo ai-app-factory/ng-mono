d96510a0057d9cf890523a14da9da7c572b79757
sukrut29 <sukrutssathaye@gmail.com>
2025-03-12 18:28:44 +0100

    add enhanced inputs for the button comp for fine grained control

--- libs/ui/mui-latest/src/lib/button/button.component.ts
+++ libs/ui/mui-latest/src/lib/button/button.component.ts
@@ -22,4 +22,28 @@ import { BaseComponent } from '../base/base.component';
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
-export class ButtonComponent {}
+export class ButtonComponent extends BaseComponent {
+  @Input() label: string = 'Button Label';
+  @Input() disabled?: boolean = false;
+  @Input() disabledInteractive?: boolean = false;
+  @Input() disableRipple?: boolean = false;
+  @Input() icon?: string = 'home';
+  @Input() buttonType:
+    'basic' |
+    'raised' |
+    'fab' |
+    'mini-fab' |
+    'stroked' |
+    'flat' |
+    'icon' |
+    'extended-fab' = 'raised';
+  @Input() onClick: () => void = () => {};
+  @Output() buttonClick = new EventEmitter<any>();
+
+  onButtonPress() {
+    this.buttonClick.emit("Button Clicked");
+    if (this.onClick) {
+      this.onClick();
+    }
+  }
+}
