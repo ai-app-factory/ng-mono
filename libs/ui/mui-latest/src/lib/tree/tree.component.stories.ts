24e2b33ad3b147e56f9c2d586f547c092f900006
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-12 15:52:52 +0100

    Update tree component and its stories

--- libs/ui/mui-latest/src/lib/tree/tree.component.stories.ts
+++ libs/ui/mui-latest/src/lib/tree/tree.component.stories.ts
@@ -1,11 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { TreeComponent } from './tree.component';
+
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
 const meta: Meta<TreeComponent> = {
   component: TreeComponent,
   title: 'TreeComponent',
+  argTypes: {
+    treeType: {
+      control: 'radio',
+      options: ['nested', 'flat'],
+      description: 'The type of tree',
+      table: {
+        type: { summary: 'nested | flat' },
+        defaultValue: { summary: 'nested' },
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<TreeComponent>;
@@ -16,8 +28,38 @@ export const Primary: Story = {
 
 export const Heading: Story = {
   args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/tree works!/gi)).toBeTruthy();
+};
+
+export const Nested: Story = {
+  args: {
+    treeType: 'nested',
+    nodes: [
+      {
+        name: 'Fruit',
+        children: [
+          { name: 'Apple' },
+          { name: 'Banana' },
+          { name: 'Fruit loops' },
+        ]
+      }, 
+      {
+        name: 'Animals',
+        children: [
+          {
+            name: 'Dogs',
+            children: [
+              { name: 'Lab' },
+              { name: 'Brreon sprouts' },
+            ]
+          }, {
+            name: 'Orange',
+            children: [
+              { name: 'Pumpkins' },
+              { name: 'Carrots' },
+            ]
+          }
+        ]
+      }
+    ]
   },
 };
