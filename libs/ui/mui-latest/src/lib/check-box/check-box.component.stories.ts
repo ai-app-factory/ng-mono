3a805a4aabb1e5ede18cf71e4203734dfcc70085
sukrut29 <sukrutssathaye@gmail.com>
2025-03-13 15:22:32 +0100

    add argTypes for input table docs and control

--- libs/ui/mui-latest/src/lib/check-box/check-box.component.stories.ts
+++ libs/ui/mui-latest/src/lib/check-box/check-box.component.stories.ts
@@ -10,6 +10,80 @@ import { expect } from '@storybook/jest';
 const meta: Meta<CheckBoxComponent> = {
   component: CheckBoxComponent,
   title: 'CheckBoxComponent',
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+      description: 'Whether the checkbox is disabled.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    labelPosition: {
+      options: ['before', 'after'],
+      control: { type: 'radio' },
+      description: 'Whether the label should appear after or before the checkbox.',
+      table: {
+        defaultValue: { summary: 'after' },
+        type: { summary: 'before | after' }
+      },
+    },
+    displayGroupLabel: {
+      control: 'boolean',
+      description: 'Whether the group label should be displayed.',
+      table: {
+        defaultValue: { summary: 'true' },
+        type: { summary: 'boolean' }
+      },
+    },
+    groupLabel: {
+      control: 'text',
+      description: 'The group label.',
+      table: {
+        defaultValue: { summary: 'To do list' },
+        type: { summary: 'string' }
+      },
+    },
+    disableRipple: {
+      control: 'boolean',
+      description: 'Whether the ripple effect should be disabled.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    disabledInteractive: {
+      control: 'boolean',
+      description: 'Whether the checkbox is disabled and not interactive.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    required: {
+      control: 'boolean',
+      description: 'Whether the checkbox is required.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    indeterminate: {
+      control: 'boolean',
+      description: 'Whether the checkbox is indeterminate.',
+      table: {
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' }
+      },
+    },
+    tasks: {
+      control: 'object',
+      description: 'The list of tasks to display.',
+      table: {
+        type: { summary: 'Task[]' }
+      },
+    }
+  }
 };
 export default meta;
 type Story = StoryObj<CheckBoxComponent>;
