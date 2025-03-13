4b67ca02c2e2bace9911b3abfd8d67d9e4a7e68a
sukrut29 <sukrutssathaye@gmail.com>
2025-03-13 20:18:15 +0100

    add more stories for ButtonComponent

--- libs/ui/mui-latest/src/lib/button/button.component.stories.ts
+++ libs/ui/mui-latest/src/lib/button/button.component.stories.ts
@@ -98,3 +98,186 @@ export const Heading: Story = {
     const canvas = within(canvasElement);
   },
 };
+
+export const WithH1Typography: Story = {
+  args: {
+    typography: "h1"
+  }
+};
+
+export const RoundedCorners: Story = {
+  args: {
+    typography: "p",
+    shape: "rounded"
+  }
+};
+
+export const SquareShaped: Story = {
+  args: {
+    typography: "p",
+    shape: "square"
+  }
+};
+
+export const PillShaped: Story = {
+  args: {
+    typography: "p",
+    shape: "pill"
+  }
+};
+
+export const Size40: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 40
+  }
+};
+
+export const CustomButtonLabel: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button"
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: true
+  }
+};
+
+export const DisabledInteractive: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: true,
+    disabledInteractive: true
+  }
+};
+
+export const RippleDisabled: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true
+  }
+};
+
+export const BasicButtonType: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "basic"
+  }
+};
+
+export const FloatingActionButton: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "fab"
+  }
+};
+
+export const MiniFloatingActionButton: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 0,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "mini-fab"
+  }
+};
+
+export const StrokedButton: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "stroked"
+  }
+};
+
+export const FlatButton: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "New Button",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "flat"
+  }
+};
+
+export const ExtendedFloatingActionButton: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "home",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "extended-fab"
+  }
+};
+
+export const CustomIconMenu: Story = {
+  args: {
+    typography: "p",
+    shape: "pill",
+    size: 16,
+    label: "home",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "icon",
+    icon: "menu"
+  }
+};
+
+export const WithH3Typography: Story = {
+  args: {
+    typography: "h3",
+    shape: "pill",
+    size: 16,
+    label: "home",
+    disabled: false,
+    disabledInteractive: true,
+    disableRipple: true,
+    buttonType: "flat",
+    icon: "menu"
+  }
+};
