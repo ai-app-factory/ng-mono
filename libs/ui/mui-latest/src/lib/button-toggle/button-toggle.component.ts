4c28d0d6a71c2f30481d11bdabb160f16bb0e0a9
sukrut29 <sukrutssathaye@gmail.com>
2025-03-13 11:28:58 +0100

    add button-toggle class for enhanced control

--- libs/ui/mui-latest/src/lib/button-toggle/button-toggle.component.ts
+++ libs/ui/mui-latest/src/lib/button-toggle/button-toggle.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+  signal
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 @Component({
   selector: 'lib-button-toggle',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './button-toggle.component.html',
   styleUrl: './button-toggle.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonToggleComponent {}
+export class ButtonToggleComponent {
+  @Input() displayGroupLabel?: boolean = false;
+  @Input() groupLabel?: string = 'Select an option';
+  @Input() selection?: 'single' | 'multiple' = 'single';
+  @Input() displaySelectedOptions?: boolean = true;
+  @Input() disabled?: boolean = false;
+  @Input() hideSelectionIndicator?: boolean = false;
+  @Input() vertical?: boolean = false;
+  @Input() disabledInteractive?: boolean = false;
+
+  @Output() valueChange = new EventEmitter<string>();
+  @Output() selectedOption = '';
+  @Output() selectedOptions = [];
+
+  @Input() set buttonToggleLabels(labels: string[]) {
+    this._buttonToggleLabels.set(labels);
+  }
+
+  protected _buttonToggleLabels = signal<string[]>(
+    [
+      'Bold',
+      'Italic',
+      'Underline'
+    ]
+  );
+
+  get buttonToggleLabels() {
+    return this._buttonToggleLabels();
+  }
+}
