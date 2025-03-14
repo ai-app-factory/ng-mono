a5cd7815fd5c62ebd6fe36915bc59e71aded3eaf
sukrut29 <sukrutssathaye@gmail.com>
2025-03-14 15:37:58 +0100

    add progress-indicator component

--- libs/ui/mui-latest/src/lib/progress-indicator/progress-indicator.component.stories.ts
+++ libs/ui/mui-latest/src/lib/progress-indicator/progress-indicator.component.stories.ts
@@ -1,11 +1,74 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { ProgressIndicatorComponent } from './progress-indicator.component';
 import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
+/**
+ * Enhanced `ProgressIndicatorComponent` which combines Material `ProgressSpinnerModule` and `ProgressBarModule`
+ * Use parameter `progressIndicatorType` to switch between spinner and bar
+ */
 const meta: Meta<ProgressIndicatorComponent> = {
   component: ProgressIndicatorComponent,
   title: 'ProgressIndicatorComponent',
+  argTypes: {
+    progressIndicatorType: {
+      options: ['bar', 'spinner'],
+      control: {type: 'select'},
+      description: 'Whether the component is a progress bar or spinner',
+      table: {
+        defaultValue: {
+          summary: 'bar'
+        },
+        type: {
+          summary: 'bar | spinner'
+        }
+      }
+    },
+    progressBarMode: {
+      options: ['determinate', 'indeterminate', 'query', 'buffer'],
+      control: {
+        type: 'select',
+      },
+      description: 'type of progress bar',
+      table: {
+        defaultValue: {summary: 'indeterminate'},
+        type: {summary: 'determinate | indeterminate | query | buffer'}
+      }
+    },
+    diameter: {
+      control: {type: 'number'},
+      description: 'Diameter of the progress spinner',
+      table: {
+        defaultValue: {summary: '40'},
+        type: {summary: 'number'}
+      },
+
+    },
+    strokeWidth: {
+      control: {type: 'number'},
+      description: 'Width of the progress spinner',
+      table: {
+        defaultValue: {summary: '1'},
+        type: {summary: 'number'}
+      },
+    },
+    progressSpinnerMode: {
+      options: ['determinate', 'indeterminate'],
+      control: {type: 'radio'},
+      description: 'Type of the progress spinner',
+      table: {
+        defaultValue: {summary: 'indeterminate'},
+        type: {summary: 'determinate | indeterminate'}
+      }
+    },
+    value: {
+      control: {type: 'number'},
+      description: 'Length of the progress bar',
+      table: {
+        defaultValue: {summary: '90'},
+        type: {summary: 'number'}
+      },
+    }
+  }
 };
 export default meta;
 type Story = StoryObj<ProgressIndicatorComponent>;
@@ -14,10 +77,67 @@ export const Primary: Story = {
   args: {},
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/progress-indicator works!/gi)).toBeTruthy();
+export const IndeterminateSpinner: Story = {
+  args: {
+    progressIndicatorType: 'spinner',
+    progressSpinnerMode: 'indeterminate',
+  },
+};
+
+export const IndeterminateBar: Story = {
+  args: {
+    progressIndicatorType: 'bar',
+    progressBarMode: 'indeterminate',
+  },
+};
+
+export const DeterminateBar: Story = {
+  args: {
+    progressIndicatorType: 'bar',
+    progressBarMode: 'determinate',
+    value: 50,
+  },
+};
+
+export const DeterminateSpinner: Story = {
+  args: {
+    progressIndicatorType: 'spinner',
+    progressSpinnerMode: 'determinate',
+    value: 50,
+  },
+};
+
+export const QueryBar: Story = {
+  args: {
+    progressIndicatorType: 'bar',
+    progressBarMode: 'query',
+    value: 50,
+  },
+};
+
+export const BufferBar: Story = {
+  args: {
+    progressIndicatorType: 'bar',
+    progressBarMode: 'buffer',
+    value: 50,
+    bufferValue: 75,
+  },
+};
+
+export const SpinnerWithDiameter100: Story = {
+  args: {
+    progressIndicatorType: 'spinner',
+    progressSpinnerMode: 'indeterminate',
+    diameter: 100,
+    strokeWidth: 5,
+  },
+};
+
+export const SpinnerWithDiameter200StrokeWidth10: Story = {
+  args: {
+    progressIndicatorType: 'spinner',
+    progressSpinnerMode: 'indeterminate',
+    diameter: 200,
+    strokeWidth: 10,
   },
 };
--- libs/ui/mui-latest/src/lib/progress-indicator/progress-indicator.component.ts
+++ libs/ui/mui-latest/src/lib/progress-indicator/progress-indicator.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule, ProgressSpinnerMode } from '@angular/material/progress-spinner';
+import { MatProgressBarModule, ProgressBarMode } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'lib-progress-indicator',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule
+  ],
   templateUrl: './progress-indicator.component.html',
   styleUrl: './progress-indicator.component.scss',
 })
-export class ProgressIndicatorComponent {}
+export class ProgressIndicatorComponent {
+  @Input() progressIndicatorType: 'spinner' | 'bar' = 'bar';
+  @Input() progressBarMode?: ProgressBarMode = 'indeterminate';
+  @Input() progressSpinnerMode?: ProgressSpinnerMode = 'indeterminate';
+  @Input() value?: number = 0;
+  @Input() diameter?: number = 50;
+  @Input() strokeWidth?: number = 2;
+  @Input() bufferValue?: number = 20;
+}
