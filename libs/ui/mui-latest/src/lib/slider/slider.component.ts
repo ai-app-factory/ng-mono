e4527fc346b1f1140d232b985fa3e2d588cae728
sukrut29 <sukrutssathaye@gmail.com>
2025-03-15 22:34:13 +0100

    add enhanced SliderComponent class

--- libs/ui/mui-latest/src/lib/slider/slider.component.ts
+++ libs/ui/mui-latest/src/lib/slider/slider.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'lib-slider',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatSliderModule,
+  ],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
-export class SliderComponent {}
+export class SliderComponent {
+  @Input() sliderType: 'basic' | 'range' = 'basic';
+  @Input() disabled?: boolean = false;
+  @Input() sliderLabel?: string = 'Select a value';
+  @Input() min?: number = 0;
+  @Input() max?: number = 100;
+  @Input() step?: number = 1;
+  @Input() thumbLabel?: boolean = false;
+  @Input() discrete?: boolean = false;
+  @Input() showSliderValues?: boolean = false;
+  @Input() displayLabel?: boolean = true;
+  @Input() showTicks?: boolean = false;
+
+
+  @Output() value: number = 0;
+  @Output() startValue: number = 0;
+  @Output() endValue: number = 0;
+
+  @Output() valueChange = new EventEmitter<number>();
+  @Output() startValueChange = new EventEmitter<number>();
+  @Output() endValueChange = new EventEmitter<number>();
+
+  protected startSlider: any;
+  protected endSlider: any;
+  protected slider: any;
+
+  onValueChange() {
+    this.valueChange.emit(this.value);
+  }
+
+  onStartValueChange() {
+    this.startValueChange.emit(this.startValue);
+  }
+
+  onEndValueChange() {
+    this.endValueChange.emit(this.endValue);
+  }
+
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    } else if (value >= 1000000) {
+      return Math.round(value / 100000) + 'M';
+    } else if (value >= 1000000000) {
+      return Math.round(value / 1000000000) + 'B';
+    }
+    return `${value}`;
+  }
+}
