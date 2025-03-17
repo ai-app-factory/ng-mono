8db5aaad2ca02b2117722c5e01420052e75ca4db
sukrut29 <sukrutssathaye@gmail.com>
2025-03-17 14:56:59 +0100

    customize table to accept any data, or coloum headers, with inbuilt sort and optional pagination

--- libs/ui/mui-latest/src/lib/table/table.component.ts
+++ libs/ui/mui-latest/src/lib/table/table.component.ts
@@ -1,7 +1,16 @@
-import { Component, AfterViewInit, ViewChild, Input  } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  Input,
+  inject
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+
 import { PeriodicElement, ELEMENT_DATA } from './data/element-data';
 
 @Component({
@@ -9,22 +18,40 @@ import { PeriodicElement, ELEMENT_DATA } from './data/element-data';
   imports: [
     CommonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
 export class TableComponent implements AfterViewInit {
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  @Input() displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  @Input() withPaginator?: boolean = true;
+  @Input() pageSizeOptions?: number[] = [5, 10, 20, 30];
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @Input() dataSource?: MatTableDataSource<any>  = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
+  @ViewChild(MatSort) sort!: MatSort;
+
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    this.dataSource!.paginator = this.paginator;
+    this.dataSource!.sort = this.sort;
   }
 
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 
 }
