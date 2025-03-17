6cea22a9da967165b2d4a79b255d252d3281c6dd
sukrut29 <sukrutssathaye@gmail.com>
2025-03-17 14:57:24 +0100

    add args table for docs and control with stories

--- libs/ui/mui-latest/src/lib/table/table.component.stories.ts
+++ libs/ui/mui-latest/src/lib/table/table.component.stories.ts
@@ -2,10 +2,69 @@ import type { Meta, StoryObj } from '@storybook/angular';
 import { TableComponent } from './table.component';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { MatTableDataSource } from '@angular/material/table';
 
 const meta: Meta<TableComponent> = {
   component: TableComponent,
   title: 'TableComponent',
+  argTypes: {
+    displayedColumns: {
+      control: {
+        type: 'object',
+      },
+      description: 'The columns to display in the table',
+      table: {
+        defaultValue: {
+          summary: 'position, name, weight, symbol',
+        },
+        type: {
+          summary: 'string[]',
+        }
+      }
+    },
+    withPaginator: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether to display the paginator',
+      table: {
+        defaultValue: {
+          summary: 'true',
+        },
+        type: {
+          summary: 'boolean',
+        }
+      }
+    },
+    pageSizeOptions: {
+      control: {
+        type: 'object',
+      },
+      description: 'The page size options for the paginator',
+      table: {
+        defaultValue: {
+          summary: '5, 10, 20, 30',
+        },
+        type: {
+          summary: 'number[]',
+        }
+      }
+    },
+    dataSource: {
+      control: {
+        type: 'object',
+      },
+      description: 'The data source for the table',
+      table: {
+        defaultValue: {
+          summary: 'ELEMENT_DATA',
+        },
+        type: {
+          summary: 'MatTableDataSource<any>',
+        }
+      }
+    },
+  }
 };
 export default meta;
 type Story = StoryObj<TableComponent>;
@@ -18,6 +77,40 @@ export const Heading: Story = {
   args: {},
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    expect(canvas.getByText(/table works!/gi)).toBeTruthy();
   },
+};
+
+export const WithoutPaginator: Story = {
+  args: {
+    withPaginator: false
+  }
+};
+
+
+
+export const WithCustomPageSizeOptions: Story = {
+  args: {
+    pageSizeOptions: [10, 20, 30]
+  }
+};
+
+interface CustomTable {
+  SrNo: number;
+  Name: string;
+  Age: number;
+  Gender: string;
+}
+
+const customData: CustomTable[] = [
+  { SrNo: 1, Name: 'John Doe', Age: 20, Gender: 'Male' },
+  { SrNo: 2, Name: 'Jane Doe', Age: 21, Gender: 'Female' },
+  { SrNo: 3, Name: 'John Smith', Age: 25, Gender: 'Male'  },
+  { SrNo: 4, Name: 'Jane Smith', Age: 26, Gender: 'Female' }
+];
+
+export const WithCustomData: Story = {
+  args: {
+    dataSource: new MatTableDataSource<CustomTable>(customData),
+    displayedColumns: ['SrNo', 'Name', 'Age', 'Gender'],
+  }
 };
