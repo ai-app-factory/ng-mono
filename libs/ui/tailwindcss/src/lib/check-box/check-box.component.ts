4fb896f60ba782ea7011ff02c4c7873fefbc9bd4
sukrut29 <sukrutssathaye@gmail.com>
2025-03-19 14:36:11 +0100

    generate check-box component

---
+++ libs/ui/tailwindcss/src/lib/check-box/check-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-check-box',
+  imports: [CommonModule],
+  templateUrl: './check-box.component.html',
+  styleUrl: './check-box.component.scss',
+})
+export class CheckBoxComponent {}
