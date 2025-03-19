180d235948f410a04ea33f0ff27ca39cc26053dc
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-19 12:51:45 +0100

    Update date picker component, Stories added

--- libs/ui/mui-latest/src/lib/date-picker/date-picker.component.stories.ts
+++ libs/ui/mui-latest/src/lib/date-picker/date-picker.component.stories.ts
@@ -6,18 +6,218 @@ import { expect } from '@storybook/jest';
 const meta: Meta<DatePickerComponent> = {
   component: DatePickerComponent,
   title: 'DatePickerComponent',
+  argTypes: {
+    toggleIcon: {
+      control: 'select',
+      options: ['keyboard_arrow_down', 'access_time', 'event', 'schedule'],
+      description: 'The icon to display on the toggle button',
+      table: {
+        type: { summary: 'keyboard_arrow_down | access_time | event | schedule' },
+        defaultValue: { summary: 'keyboard_arrow_down' },
+      },
+    },
+    datePickerType: {
+      control: 'select',
+      options: ['single', 'range'],
+      description: 'The type of date picker to display',
+      table: {
+        type: { summary: 'single | range' },
+        defaultValue: { summary: 'single' },
+      },
+    },
+    actionButton: {
+      control: 'boolean',
+      description: 'Whether to display an action buttons',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    label: {
+      control: 'text',
+      description: 'The label for the date picker',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Pick a date' },
+      },
+    },
+    openMethod: {
+      control: 'select',
+      options: ['normal', 'button'],
+      description: 'How to open the date picker',
+      table: {
+        type: { summary: 'normal | button' },
+        defaultValue: { summary: 'normal' },
+      },
+    },
+    disableInput: {
+      control: 'boolean',
+      description: 'Whether to disable the input',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    disableToggle: {
+      control: 'boolean',
+      description: 'Whether to disable the toggle button',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+    locale: {
+      control: 'select',
+      options: ['en-US', 'bg-BG', 'fr-FR', 'de-DE'],
+      description: 'The locale for the date picker',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'en-US' },
+      },
+    },
+    minDate: {
+      control: 'text',
+      description: 'The minimum date',
+      table: {
+        type: { summary: 'string | Date' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    maxDate: {
+      control: 'text',
+      description: 'The maximum date',
+      table: {
+        type: { summary: 'string | Date' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    startDate: {
+      control: 'text',
+      description: 'The start date',
+      table: {
+        type: { summary: 'string | Date' },
+        defaultValue: { summary: 'null' },
+      },
+    },
+
+    startView: {
+      control: 'select',
+      options: ['month', 'year', 'multi-year'],
+      description: 'The start view for the date picker',
+      table: {
+        type: { summary: 'month | year | multi-year' },
+        defaultValue: { summary: 'month' },
+      },
+    },
+    touchUi: {
+      control: 'boolean',
+      description: 'Whether to use touch UI',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' },
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<DatePickerComponent>;
 
-export const Primary: Story = {
-  args: {},
+export const BasicDatePicker: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    locale: 'en-US',
+  },
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/date-picker works!/gi)).toBeTruthy();
+export const BasicDateRangePicker: Story = {
+  args: {
+    label: 'Pick a date range',
+    datePickerType: 'range',
+    locale: 'en-US',
+  },
+};
+
+export const DatePickerWithActionButtons: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    actionButton: true,
+    locale: 'en-US',
+  },
+};
+
+export const DatePickerWithToggleButton: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    openMethod: 'button',
+    locale: 'en-US',
+  },
+};
+
+export const DatePickerWithCustomIcon: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    toggleIcon: 'event',
+    locale: 'en-US',
+  },
+};
+
+export const DisabledDatePicker: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    disableInput: true,
+    disableToggle: true,
+    locale: 'en-US',
+  },
+};
+
+export const WithDifferentLocales: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    locale: 'bg-BG',
+  },
+};
+
+
+
+export const DatePickerWithMinMaxValidation: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    locale: 'en-US',
+    minDate: new Date(2023, 0, 1), // January 1, 2023
+    maxDate: new Date(2023, 11, 31), // December 31, 2023
+  },
+};
+
+export const WithStartDate: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    locale: 'en-US',
+    startDate: new Date(2023, 10, 15), // June 15, 2023
+  },
+};
+
+export const DatePickerTouchUI: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    touchUi: true,
+    locale: 'en-US',
+  },
+};
+
+export const DatePickerForDifferentStartView: Story = {
+  args: {
+    label: 'Pick a date',
+    datePickerType: 'single',
+    startView: 'year',
+    locale: 'en-US',
   },
 };
--- libs/ui/mui-latest/src/lib/date-picker/date-picker.component.ts
+++ libs/ui/mui-latest/src/lib/date-picker/date-picker.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import {ChangeDetectionStrategy, Component, OnDestroy, signal, inject, Input} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
-import {DateAdapter, MAT_DATE_FORMATS, provideNativeDateAdapter} from '@angular/material/core';
+import {DateAdapter, MAT_DATE_FORMATS, provideNativeDateAdapter, MAT_DATE_LOCALE} from '@angular/material/core';
 import {MatCalendar, MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {Subject} from 'rxjs';
 import {startWith, takeUntil} from 'rxjs/operators';
+
 
 @Component({
   selector: 'lib-date-picker',
@@ -16,7 +17,9 @@ import {startWith, takeUntil} from 'rxjs/operators';
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule
   ],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss',
@@ -24,4 +27,44 @@ import {startWith, takeUntil} from 'rxjs/operators';
   providers: [provideNativeDateAdapter()],
 })
 export class DatePickerComponent {
+  @Input() toggleIcon: string = 'keyboard_arrow_down';
+  @Input() datePickerType: 'single' | 'range'  = 'single';
+  @Input() actionButton: boolean = false;
+  @Input() label: string = 'Pick a date';
+  @Input() openMethod: 'normal' | 'button' = 'normal';
+
+  @Input() disableInput: boolean = false;
+  @Input() disableToggle: boolean = false;
+
+  @Input() minDate: string | Date | null = null;
+  @Input() maxDate: string | Date | null = null;
+
+  @Input() startDate: string | Date | null = null;
+
+  @Input() startView: 'month' | 'year' | 'multi-year' = 'month';
+
+  @Input() touchUi: boolean = false;
+
+  @Input() set locale(value: string) {
+    this._adapter.setLocale(value);
+    this._locale = value;
+  }
+
+  private _adapter = inject<DateAdapter<unknown>>(DateAdapter);
+  private _locale: string = 'fr-FR';
+
+  get dateFromatString(): string {
+    switch (this._locale) {
+      case 'en-US':
+        return 'MM/YY/YYYY';
+      case 'bg-BG':
+        return 'DD/MM/YYYY';
+      case 'fr-FR':
+        return 'DD/MM/YYYY';
+      case 'de-DE':
+        return 'DD.MM.YYYY';
+      default:
+        return 'DD/MM/YYYY';
+    }
+  }
 }
