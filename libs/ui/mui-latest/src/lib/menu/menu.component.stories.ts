8fd50751732d3abbdac554b4a21cc524568e69c3
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-20 22:13:17 +0100

    Update menu component

--- libs/ui/mui-latest/src/lib/menu/menu.component.stories.ts
+++ libs/ui/mui-latest/src/lib/menu/menu.component.stories.ts
@@ -1,23 +1,69 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { MenuComponent } from './menu.component';
+
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
 const meta: Meta<MenuComponent> = {
   component: MenuComponent,
   title: 'MenuComponent',
+  argTypes: {
+    menuTitle: {
+      control: 'text',
+      description: 'The title of the menu',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Menu' },
+      }
+    },
+    menuIcon: {
+      control: 'text',
+      description: 'The icon of the menu',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'more_vert' },
+      }
+    },
+  },
 };
+
+
 export default meta;
 type Story = StoryObj<MenuComponent>;
 
-export const Primary: Story = {
-  args: {},
+export const BasicMenu: Story = {
+  args: {
+    menus: [
+      {
+        label: 'HomePage',
+      },
+      {
+        label: 'About Us',
+      },
+      {
+        label: 'Contact Us',
+      }
+    ]
+  },
 };
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/menu works!/gi)).toBeTruthy();
+export const MenuWithIcons: Story = {
+  args: {
+    menuType: 'icon',
+    menus: [
+      {
+        label: 'HomePage',
+        icon: 'home',
+      },
+      {
+        label: 'Check voice mail',
+        icon: 'voicemail',
+        disabled: true,
+      },
+      {
+        label: 'Disable alerts',
+        icon: 'notifications_off',
+      }
+    ]
   },
 };
--- libs/ui/mui-latest/src/lib/menu/menu.component.ts
+++ libs/ui/mui-latest/src/lib/menu/menu.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatMenu, MatMenuModule, MatMenuPanel} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ButtonComponent } from '../button/button.component';
+
+export interface Menu {
+  label: string;
+  icon?: string;
+  link?: string;
+  disabled?: boolean;
+  role?: 'menuitem' | 'menuitemradio' | 'menuitemcheckbox';
+  disabledRipple?: boolean;
+  menuTrigger?: string;
+  children?: Menu[];
+}
+
+export type MenuCloseReason = void | 'click' | 'keydown' | 'tab';
 
 @Component({
   selector: 'lib-menu',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    ButtonComponent
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @Input() menuTitle?: string = 'Menu';
+  @Input() menuIcon?: string = 'more_vert';
+  @Input() menuType: 'icon' | 'nested' | 'basic' = 'basic';
+
+
+
+  protected _menus = signal<Menu[]>([
+    {
+      label: 'Home',
+      icon: 'home',
+      link: '/',
+    },
+    {
+      label: 'About',
+      icon: 'info',
+      link: '/about',
+    },
+    {
+      label: 'Contact',
+      icon: 'contact',
+      link: '/contact',
+      children: [
+        {
+          label: 'Email',
+          link: '/contact/email',
+          icon: 'email',
+        },
+        {
+          label: 'Phone',
+          link: '/contact/phone',
+          icon: 'phone',
+        },
+        {
+          label: 'Fax',
+          link: '/contact/fax',
+          icon: 'fax',
+          disabled: true,
+        }
+      ]
+    },
+  ]);
+
+  
+  @Input() xPosition: 'before' | 'after' = 'after';
+  @Input() yPosition: 'above' | 'below' = 'below';
+  @Input() overlapTrigger: boolean = false;
+
+  protected _menuTrigger = signal<MatMenuPanel<any> | null>(null);
+  @Input() disabled: boolean = false;
+  @Output() closed: EventEmitter<MenuCloseReason>
+    = new EventEmitter<MenuCloseReason>();
+
+  @Input() set menus(menus: Menu[]) {
+    this._menus.set(menus);
+  }
+
+  @Input() set menuTrigger(trigger: MatMenuPanel<any>) {
+    this._menuTrigger.set(trigger);
+  }
+
+  // @ViewChild('menu', { static: true }) menu!: MatMenu;
+
+}
