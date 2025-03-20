91e1578856349cbd84dd9c6e9dd92b0cafff2b99
sukrut29 <sukrutssathaye@gmail.com>
2025-03-20 16:11:54 +0100

    wip: use tw4 to style button

--- libs/ui/tailwindcss/src/lib/button/button.component.stories.ts
+++ libs/ui/tailwindcss/src/lib/button/button.component.stories.ts
@@ -6,18 +6,155 @@ import { expect } from '@storybook/jest';
 const meta: Meta<ButtonComponent> = {
   component: ButtonComponent,
   title: 'ButtonComponent',
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+      description: 'The label for the button',
+      table: {
+        defaultValue: {
+          summary: 'Button with Tailwind',
+        },
+        type: {
+          summary: 'string',
+        }
+      }
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Disables the button',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        }
+      }
+    },
+    disableRipple: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Disables the ripple effect',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        }
+      }
+    },
+    disabledInteractive: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Disables the interactive state',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    icon: {
+      control: {
+        type: 'text',
+      },
+      description: 'The icon for the button',
+      table: {
+        defaultValue: {
+          summary: 'home',
+        },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    buttonType: {
+      options: ['raised', 'flat', 'stroked', 'fab', 'extended-fab'],
+      control: {
+        type: 'select',
+      },
+      description: 'The type of button',
+      table: {
+        defaultValue: {
+          summary: 'raised',
+        },
+        type: {
+          summary: 'raised | flat | stroked | fab | extended-fab',
+        },
+      },
+    },
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: {
+        type: 'select',
+      },
+      description: 'The size of the button',
+      table: {
+        defaultValue: {
+          summary: 'medium',
+        },
+        type: {
+          summary: 'small | medium | large',
+        },
+      },
+    },
+    shape: {
+      options: ['pill', 'round', 'rectangle'],
+      control: {
+        type: 'select',
+      },
+      description: 'The shape of the button',
+      table: {
+        defaultValue: {
+          summary: 'pill',
+        },
+        type: {
+          summary: 'pill | round | rectangle',
+        },
+      },
+    },
+    color: {
+      options: ['primary', 'accent', 'warn'],
+      control: {
+        type: 'select',
+      },
+      description: 'The color of the button',
+      table: {
+        defaultValue: {
+          summary: 'primary',
+        },
+        type: {
+          summary: 'primary | accent | warn',
+        },
+      },
+    },
+    onButtonClick: {
+      action: 'onButtonClick',
+      description: 'The function to call when the button is clicked',
+      table: {
+        type: {
+          summary: '() => any',
+        },
+      },
+    },
+  }
 };
 export default meta;
 type Story = StoryObj<ButtonComponent>;
 
-export const Primary: Story = {
-  args: {},
-};
 
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/button works!/gi)).toBeTruthy();
-  },
-};
+export const PillShape: Story = {
+  args: {
+    shape: 'pill',
+    color: 'primary',
+  }
+}
--- libs/ui/tailwindcss/src/lib/button/button.component.ts
+++ libs/ui/tailwindcss/src/lib/button/button.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'lib-button',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatButton
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() label: string = 'Button with Tailwind';
+  @Input() disabled?: boolean = false;
+  @Input() disableRipple?: boolean = false;
+  @Input() disabledInteractive?: boolean = false;
+  @Input() icon?: string = 'home';
+
+  @Input() buttonType?:
+    'raised' |
+    'flat' |
+    'stroked' |
+    'fab' |
+    'extended-fab' = 'raised';
+
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+  @Input() shape: 'pill' | 'round' | 'rectangle' = 'pill';
+  @Input() color: 'primary' | 'accent' | 'warn' = 'primary';
+
+  @Input() onButtonClick?: () => any = () => {};
+
+  @Output() buttonClick = new EventEmitter<string>();
+
+  onClick() {
+    this.buttonClick.emit('Button Clicked');
+    if (this.onButtonClick) {
+      this.onButtonClick();
+    }
+  }
+
+  // get buttonClasses() {
+  //   const shapeClass = this.shape ? `button-${this.shape}` : '';
+  //   const sizeClass = this.size ? `button-${this.size}` : '';
+  //   const colorClass = this.color ? `button-${this.color}` : '';
+  //   return [shapeClass, sizeClass, colorClass];
+  // }
+
+
+}
