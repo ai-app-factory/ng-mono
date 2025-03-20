e646c5811e349abb52a99a98a0c804373f91a6e0
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-03-20 18:40:49 +0100

    Update Select component and add additional stories

--- libs/ui/mui-latest/src/lib/select/select.component.stories.ts
+++ libs/ui/mui-latest/src/lib/select/select.component.stories.ts
@@ -6,18 +6,82 @@ import { expect } from '@storybook/jest';
 const meta: Meta<SelectComponent> = {
   component: SelectComponent,
   title: 'SelectComponent',
+  argTypes: {
+    disabled: { control: 'boolean' },
+  },
 };
 export default meta;
 type Story = StoryObj<SelectComponent>;
 
-export const Primary: Story = {
-  args: {},
-};
-
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/select works!/gi)).toBeTruthy();
+export const BasicSelect: Story = {
+  args: {
+    label: 'Select an option',
+    placeholder: 'Please select an option',
   },
 };
+
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const WithMultipleSelections: Story = {
+  args: {
+    multiple: true,
+  },
+};
+
+export const WithDisabledOptions: Story = {
+  args: {
+    groups: [
+      { id: 1, value: 'Apple' },
+      { id: 2, value: 'Banana', disabled: true },
+      { id: 3, value: 'Cherry' },
+      { id: 4, value: 'Date', disabled: true },
+    ],
+  },
+};
+
+export const WithGroups: Story = {
+  args: {
+    option: 'nested',
+    groups: [
+      {
+        id: 1,
+        value: 'Grass',
+        groups: [
+          { id: 2, value: 'Bulbasaur' },
+          { id: 3, value: 'Oddish' },
+        ],
+      },
+      {
+        id: 4,
+        value: 'Fire',
+        groups: [
+          { id: 5, value: 'Charmander' },
+          { id: 6, value: 'Vulpix' },
+        ],
+      },
+      {
+        id: 7,
+        value: 'Water',
+        groups: [
+          { id: 8, value: 'Squirtle' },
+          { id: 9, value: 'Psyduck' },
+        ],
+      },
+      {
+        id: 10,
+        value: 'Electric',
+        groups: [
+          { id: 11, value: 'Pikachu' },
+          { id: 12, value: 'Jolteon' },
+        ],
+        disabled: true,
+      }
+    ],
+  },
+};
+
--- libs/ui/mui-latest/src/lib/select/select.component.ts
+++ libs/ui/mui-latest/src/lib/select/select.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface Group {
+  id: number;
+  value: string;
+  groups?: Group[];
+  disabled?: boolean;
+}
 
 @Component({
   selector: 'lib-select',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss',
 })
-export class SelectComponent {}
+export class SelectComponent {
+
+  @Input() disabled = false;
+
+  @Input() placeholder = 'Please select an option';
+
+  @Input() label = 'Select an option';
+
+  @Input() multiple = false;
+
+  @Input() option: 'nested' | 'flat' = 'flat';
+
+  @Input() set groups(groups: Group[]) {
+    this._groups.set(groups);
+  }
+
+  protected _groups = signal<Group[]>([
+    {
+      id: 1,
+      value: 'Apple',
+    },
+    {
+      id: 2,
+      value: 'Banana',
+    },
+    {
+      id: 3,
+      value: 'Cherry',
+    },
+    {
+      id: 4,
+      value: 'Date',
+    }
+  ]);
+}
+
+
