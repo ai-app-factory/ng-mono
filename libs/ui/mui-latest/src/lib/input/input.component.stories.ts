3b90c3b90adac0d131e7e2e2245f2e5407cc29c1
sukrut29 <sukrutssathaye@gmail.com>
2025-03-21 09:10:14 +0100

    create enhanced input component with form-field. Requires refinement

--- libs/ui/mui-latest/src/lib/input/input.component.stories.ts
+++ libs/ui/mui-latest/src/lib/input/input.component.stories.ts
@@ -3,9 +3,199 @@ import { InputComponent } from './input.component';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
+/**
+ * `InputComponent` with inbuilt form validation for email and password.
+ * Additional features include `clearable`, `disabled`, `disabledInteractive`, `hint`, `icon`, `label`, `labelAppearance`, `placeholder`, `type`, and `value`.
+ */
 const meta: Meta<InputComponent> = {
   component: InputComponent,
   title: 'InputComponent',
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+      description: 'The label for the input field.',
+      table: {
+        defaultValue: {
+          summary: 'Type here',
+        },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+      description: 'The placeholder for the input field.',
+      table: {
+        defaultValue: {
+          summary: 'Placeholder',
+        },
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the input field is disabled.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    disabledInteractive: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the input field is disabled and interactive.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    value: {
+      control: {
+        type: 'text',
+      },
+      description: 'The value of the input field.',
+      table: {
+        defaultValue: {
+          summary: '',
+        },
+        type: {
+          summary: 'string'
+        },
+      },
+    },
+    clearable: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the input field is clearable.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean'
+        },
+      },
+    },
+    hint: {
+      control: {
+        type: 'text',
+      },
+      description: 'The hint for the input field.',
+      table: {
+        defaultValue: {
+          summary: '',
+        },
+        type: {
+          summary: 'string'
+        },
+      },
+    },
+    icon: {
+      control: {
+        type: 'text',
+      },
+      description: 'The icon for the input field.',
+      table: {
+        defaultValue: {
+          summary: '',
+        },
+        type: {
+          summary: 'string'
+        },
+      },
+    },
+    labelAppearance: {
+      options: ['fill', 'outline'],
+      control: {
+        type: 'select',
+      },
+      description: 'The appearance of the label.',
+      table: {
+        defaultValue: {
+          summary: 'fill',
+        },
+        type: {
+          summary: 'fill | outline',
+        },
+      },
+    },
+    type: {
+      options: ['text', 'number', 'password', 'email', 'tel', 'url', 'search', 'date', 'time', 'datetime-local', 'month', 'color', 'text-area'],
+      control: {
+        type: 'select',
+      },
+      description: 'The type of the input field.',
+      table: {
+        defaultValue: {
+          summary: 'text',
+        },
+        type: {
+          summary: 'InputType',
+        },
+      },
+    },
+    maxLength: {
+      control: {
+        type: 'text',
+      },
+      description: 'The maximum length of the input field.',
+      table: {
+        defaultValue: {
+          summary: '100',
+        },
+        type: {
+          summary: 'string'
+        },
+      },
+    },
+    minLength: {
+      control: {
+        type: 'text',
+      },
+      description: 'The minimum length of the input field.',
+      table: {
+        defaultValue: {
+          summary: '0',
+        },
+        type: {
+          summary: 'string'
+        },
+      },
+    },
+    required: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the input field is required.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean'
+        },
+      },
+    }
+  }
 };
 export default meta;
 type Story = StoryObj<InputComponent>;
--- libs/ui/mui-latest/src/lib/input/input.component.ts
+++ libs/ui/mui-latest/src/lib/input/input.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  signal,
+  ChangeDetectionStrategy
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormControl,
+  Validators
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FloatLabelType,
+  MatFormField,
+  MatFormFieldAppearance,
+  MatFormFieldModule
+} from '@angular/material/form-field';
 
+type InputType =
+  'text' |
+  'number' |
+  'password' |
+  'email' |
+  'tel' |
+  'url' |
+  'search' |
+  'date' |
+  'time' |
+  'datetime-local' |
+  'month' |
+  'color' |
+  'text-area';
 @Component({
   selector: 'lib-input',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() label?: string = 'Type here';
+  @Input() placeholder?: string = 'Placeholder';
+  @Input() disabled?: boolean = false;
+  @Input() disabledInteractive?: boolean = false;
+  @Input() value?: string = '';
+  @Input() type?: InputType = 'text';
+  @Input() hint?: string = '';
+  @Input() icon?: string = '';
+  @Input() labelAppearance?: MatFormFieldAppearance = 'fill';
+  @Input() clearable?: boolean = false;
+  @Input() maxLength?: string = '100';
+  @Input() minLength?: string = '0';
+  @Input() hintAlign?: 'start' | 'end' = 'start';
+  @Input() readonly?: boolean = false;
+  @Input() required?: boolean = false;
+  @Input() isPrefix?: boolean = false;
+  @Input() isSuffix?: boolean = false;
+  @Input() prefixType?: 'icon' | 'text';
+  @Input() suffixType?: 'icon' | 'text';
+  @Input() prefixText?: string = '';
+  @Input() suffixText?: string = '';
+  @Input() prefixIcon?: string = '';
+  @Input() suffixIcon?: string = '';
+  @Input() floatLabel?: FloatLabelType = 'auto';
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  protected hidePassword = signal(true);
+
+  protected emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  protected passwordFormControl = new FormControl('', [
+    Validators.minLength(8),
+    Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+  ]);
+
+  onValueChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.value = inputElement.value?.trim();
+    this.valueChange.emit(this.value);
+  }
+
+  toggleShowPassword(event: MouseEvent) {
+    this.hidePassword.set(!this.hidePassword());
+    event.stopPropagation();
+  }
+}
