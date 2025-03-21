0e3a86d666fa37744945a4cab71c1bb29145917d
sukrut29 <sukrutssathaye@gmail.com>
2025-03-21 11:51:44 +0100

    add NoopAnimationModule

--- libs/ui/mui-latest/src/lib/time-picker/time-picker.component.spec.ts
+++ libs/ui/mui-latest/src/lib/time-picker/time-picker.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TimePickerComponent } from './time-picker.component';
-
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 describe('TimePickerComponent', () => {
   let component: TimePickerComponent;
   let fixture: ComponentFixture<TimePickerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TimePickerComponent],
+      imports: [
+        TimePickerComponent,
+        BrowserAnimationsModule,
+        NoopAnimationsModule
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TimePickerComponent);
