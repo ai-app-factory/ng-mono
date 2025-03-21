bf9ba35c9af68f13afe7c6ae8d803c9d876b0418
sukrut29 <sukrutssathaye@gmail.com>
2025-03-21 09:10:30 +0100

    add argtypes for docs and control for list

--- libs/ui/mui-latest/src/lib/list/list.component.stories.ts
+++ libs/ui/mui-latest/src/lib/list/list.component.stories.ts
@@ -6,7 +6,91 @@ const meta: Meta<ListComponent> = {
   component: ListComponent,
   title: 'ListComponent',
   argTypes: {
-
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the list is disabled.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    disableRipple: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether the list should have a ripple effect.',
+      table: {
+        defaultValue: {
+          summary: 'false',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    listType: {
+      options: ['list', 'selection'],
+      control: {
+        type: 'select',
+      },
+      description: 'The type of list to render.',
+      table: {
+        defaultValue: {
+          summary: 'list',
+        },
+        type: {
+          summary: 'list | selection',
+        },
+      },
+    },
+    lines: {
+      control: {
+        type: 'number',
+      },
+      description: 'The number of lines to display for each list item.',
+      table: {
+        defaultValue: {
+          summary: '1',
+        },
+        type: {
+          summary: 'number',
+        },
+      },
+    },
+    multiple: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Whether multiple items can be selected.',
+      table: {
+        defaultValue: {
+          summary: 'true',
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    listItems: {
+      control: {
+        type: 'object',
+      },
+      description: 'The list items to display.',
+      table: {
+        defaultValue: {
+          summary: '[]',
+        },
+        type: {
+          summary: 'ListItem[]',
+        },
+      },
+    }
   },
 };
 export default meta;
