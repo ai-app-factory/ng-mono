4625177ac6f7eae3316dc4b8121aac521e12931a
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-04-04 10:21:52 +0200

    Create Alert Component For clarity design

--- libs/ui/clarity-design/src/index.ts
+++ libs/ui/clarity-design/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/clarity-design/clarity-design.component';
 export * from './lib/accordian/accordian.component';
 
 export * from './lib/accordian/accordian.component';
+
+export * from './lib/alert/alert.component';
---
+++ libs/ui/clarity-design/src/lib/alert/alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-alert',
+  imports: [CommonModule],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss',
+})
+export class AlertComponent {}
