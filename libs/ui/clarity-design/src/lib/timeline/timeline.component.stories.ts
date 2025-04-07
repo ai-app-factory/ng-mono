3fe5649821037bfa04b1b72ccbf026b23852e83b
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-04-07 11:57:37 +0200

    Update Timeline COmponent: Add basic features to the timeline component

--- libs/ui/clarity-design/src/lib/timeline/timeline.component.stories.ts
+++ libs/ui/clarity-design/src/lib/timeline/timeline.component.stories.ts
@@ -6,12 +6,93 @@ import { expect } from '@storybook/jest';
 const meta: Meta<TimelineComponent> = {
   component: TimelineComponent,
   title: 'TimelineComponent',
+  argTypes: {
+    layout: {
+      control: {
+        type: 'select',
+        options: ['HORIZONTAL', 'VERTICAL'],
+      },
+      defaultValue: 'HORIZONTAL',
+    },
+    steps: {
+      control: {
+        type: 'object',
+      },
+      defaultValue: [
+        {
+          title: 'Step 1',
+          description: 'Description for step 1',
+          header: 'Header for step 1',
+          state: 'NOT_STARTED',
+        },
+        {
+          title: 'Step 2',
+          description: 'Description for step 2',
+          header: 'Header for step 2',
+          state: 'CURRENT',
+        },
+        {
+          title: 'Step 3',
+          description: 'Description for step 3',
+          header: 'Header for step 3',
+          state: 'SUCCESS',
+        },
+        {
+          title: 'Step 4',
+          description: 'Description for step 4',
+          header: 'Header for step 4',
+          state: 'ERROR',
+        },
+        {
+          title: 'Step 5',
+          description: 'Description for step 5',
+          header: 'Header for step 5',
+          state: 'PROCESSING',
+        },
+      ],
+    },
+  },
 };
+
 export default meta;
 type Story = StoryObj<TimelineComponent>;
 
 export const Primary: Story = {
-  args: {},
+  args: {
+    layout: 'VERTICAL',
+    steps: [
+      {
+        title: 'Step 1',
+        description: 'Description for step 1',
+        header: 'Header for step 1',
+        state: 'NOT_STARTED',
+      },
+      {
+        title: 'Step 2',
+        description: 'Description for step 2',
+        header: 'Header for step 2',
+        state: 'CURRENT',
+      },
+      {
+        title: 'Step 3',
+        description: 'Description for step 3',
+        header: 'Header for step 3',
+        state: 'SUCCESS',
+      },
+      {
+        title: 'Step 4',
+        description: 'Description for step 4',
+        header: 'Header for step 4',
+        state: 'ERROR',
+      },
+      {
+        title: 'Step 5',
+        description: 'Description for step 5',
+        header: 'Header for step 5',
+        state: 'PROCESSING',
+      },
+    ],
+  },
 };
 
 export const Heading: Story = {
--- libs/ui/clarity-design/src/lib/timeline/timeline.component.ts
+++ libs/ui/clarity-design/src/lib/timeline/timeline.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  Input,
+  Output,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClarityModule, ClrTimelineStepState, ClrTimelineLayout } from '@clr/angular';
+
+
+export interface TimelineStep {
+  title: string;
+  description: string;
+  header: string;
+  state: 'NOT_STARTED' | 'CURRENT' | 'SUCCESS' | 'ERROR' | 'PROCESSING';
+}
 
 @Component({
   selector: 'lib-timeline',
-  imports: [CommonModule],
+  imports: [CommonModule, ClarityModule],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TimelineComponent {}
+export class TimelineComponent {
+  @Input() layout: 'HORIZONTAL' | 'VERTICAL' = 'HORIZONTAL';
+
+  protected _steps = signal<TimelineStep[]>([]);
+
+  @Input() set steps(steps: TimelineStep[]) {
+    this._steps.set(steps);
+  }
+
+  get clrLayout(): ClrTimelineLayout {
+    return this.layout === 'HORIZONTAL' ? ClrTimelineLayout.HORIZONTAL : ClrTimelineLayout.VERTICAL;
+  }
+}
