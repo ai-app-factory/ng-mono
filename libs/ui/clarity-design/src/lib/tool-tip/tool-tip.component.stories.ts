af8a491c848c33de598c00b11558e6ae3b966aef
Sudarshan Nanjundegowda <sudarshan.ng7@gmail.com>
2025-04-07 14:19:08 +0200

    Update Tool-tip component: Add signpost and tool-tip features to the component

--- libs/ui/clarity-design/src/lib/tool-tip/tool-tip.component.stories.ts
+++ libs/ui/clarity-design/src/lib/tool-tip/tool-tip.component.stories.ts
@@ -6,18 +6,53 @@ import { expect } from '@storybook/jest';
 const meta: Meta<ToolTipComponent> = {
   component: ToolTipComponent,
   title: 'ToolTipComponent',
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['Tool-Tip', 'SignPost'],
+      description: 'Type of the tool tip',
+      table: {
+        type: { summary: 'string'},
+        defaultValue: { summary: 'Tool-Tip' }, 
+        },
+      },
+    config: {
+      control: 'object',
+      description: 'Configuration for the tool tip',
+      table: {
+        type: { summary: 'ToolTipSignPostConfig' },
+        defaultValue: { summary: '{ content: "This is a tool tip", position: "top", tooltipSize: "xs", icon: "info-standard", iconSize: "24" }' },
+      },
+    },
+  },
+
 };
 export default meta;
 type Story = StoryObj<ToolTipComponent>;
 
-export const Primary: Story = {
-  args: {},
-};
-
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/tool-tip works!/gi)).toBeTruthy();
+export const Default: Story = {
+  args: {
+    type: 'Tool-Tip',
+    config: {
+      content: 'This is a tool tip',
+      position: 'top-right',
+      tooltipSize: 'xs',
+      icon: 'info-standard',
+      iconSize: '48',
+    },
   },
 };
+
+export const SignPost: Story = {
+  args: {
+    type: 'SignPost',
+    config: {
+      content: 'This is a sign post',
+      position: 'right-middle',
+      tooltipSize: 'lg',
+      icon: 'info-standard',
+      iconSize: '48',
+    },
+  },
+};
+
--- libs/ui/clarity-design/src/lib/tool-tip/tool-tip.component.ts
+++ libs/ui/clarity-design/src/lib/tool-tip/tool-tip.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  Input,
+  Output, 
+  signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
+import { ClarityIcons, userIcon, alarmClockIcon, airplaneIcon } from '@cds/core/icon';
+
+
+export interface ToolTipSignPostConfig {
+  content: string;
+  position: 'top' | 'bottom' | 'left' | 'right' | 'top-left' | 'top-right' | 'top-moddle' | 'bottom-left' | 'bottom-right' | 'right-middle' | 'left-middle';
+  icon?: string;
+  iconSize?: string | number;
+  tooltipSize: 'xs' | 'md' | 'sm' | 'lg';
+}
 
 @Component({
   selector: 'lib-tool-tip',
-  imports: [CommonModule],
+  imports: [CommonModule, ClarityModule],
   templateUrl: './tool-tip.component.html',
   styleUrl: './tool-tip.component.scss',
 })
-export class ToolTipComponent {}
+export class ToolTipComponent {
+  @Input() type: 'Tool-Tip' | 'SignPost' = 'Tool-Tip';
+
+  protected _config = signal<ToolTipSignPostConfig>({
+    content: 'This is a tool tip',
+    position: 'top',
+    tooltipSize: 'xs',
+    icon: 'info-standard',
+    iconSize: "48",
+  });
+  @Input() set config(value: ToolTipSignPostConfig) {
+    this._config.set(value);
+  }
+  get config(): ToolTipSignPostConfig {
+    return this._config();
+  }
+}
